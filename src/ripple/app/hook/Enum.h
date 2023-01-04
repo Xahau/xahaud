f70ddf9e65419e5c8825708da220bf80f693ce2e
Richard Holland <richard.holland@starstone.co.nz>
2023-01-04 11:36:01 +0000

    add otxn_param hook api

--- src/ripple/app/hook/Enum.h
+++ src/ripple/app/hook/Enum.h
@@ -545,6 +545,7 @@ namespace hook_api
         "otxn_generation",
         "otxn_id",
         "otxn_type",
+        "otxn_param",
         "rollback",
         "slot",
         "slot_clear",
--- src/test/app/SetHook_test.cpp
+++ src/test/app/SetHook_test.cpp
@@ -5147,6 +5147,11 @@ public:
     }
 
     void
+    test_otxn_param()
+    {
+    }
+    
+    void
     test_slot()
     {
         testcase("Test slot");
@@ -9703,6 +9708,7 @@ public:
         test_otxn_id();             //
         test_otxn_slot();           //
         test_otxn_type();           //
+        test_otxn_param();          
 
         test_slot();                //
         test_slot_clear();          //
