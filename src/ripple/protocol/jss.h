c193fd58af2d13fa02e4cafed5dc5a82fa689de7
Richard Holland <richard.holland@starstone.co.nz>
2023-01-13 11:21:58 +0000

    remove duplicate jss entries

--- src/ripple/protocol/jss.h
+++ src/ripple/protocol/jss.h
@@ -394,9 +394,7 @@ JSS(local);                       // out: resource/Logic.h
 JSS(local_txs);                   // out: GetCounts
 JSS(local_static_keys);           // out: ValidatorList
 JSS(locked_balance);              // out: AccountLines
-JSS(lock_count);                  // out: AccountLines
 JSS(low);                         // out: BookChanges
-JSS(locked_balance);              // out: AccountLines
 JSS(lock_count);                  // out: AccountLines
 JSS(lowest_sequence);             // out: AccountInfo
 JSS(lowest_ticket);               // out: AccountInfo
