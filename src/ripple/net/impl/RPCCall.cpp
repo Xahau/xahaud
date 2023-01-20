d492ffd04ef7e21c1931bad930cac063de1039ae
dangell7 <denisangell7@gmail.com>
2023-01-20 00:40:20 -0500

    cleanup and fix rpc call test

--- src/ripple/net/impl/RPCCall.cpp
+++ src/ripple/net/impl/RPCCall.cpp
@@ -886,10 +886,8 @@ private:
             
             // parse string
             Json::Value amountJson = jvParseSTAmount(jvParams[index].asString());
-            // std::cout << "AMT JSON: " << amountJson << "\n";
             if (!amountJson) {
                 // amount is string
-                // std::cout << "IS STRING: " << "\n";
                 if (!to_uint64(jvParams[index].asString()))
                     return rpcError(rpcCHANNEL_AMT_MALFORMED);
                 
@@ -898,7 +896,6 @@ private:
             else
             {
                 // amount is json
-                // std::cout << "IS JSON: " << "\n";
                 STAmount amount;
                 bool isAmount = amountFromJsonNoThrow(amount, amountJson);
                 if (!isAmount)
@@ -939,7 +936,6 @@ private:
                 return rpcError(rpcCHANNEL_AMT_MALFORMED);
             // parse string
             Json::Value amountJson = jvParseSTAmount(jvParams[2u].asString());
-            // std::cout << "AMT JSON: " << amountJson << "\n";
             if (!amountJson) {
                 // amount is string
                 if (!to_uint64(jvParams[2u].asString()))
@@ -960,7 +956,6 @@ private:
         }
 
         jvRequest[jss::signature] = jvParams[3u].asString();
-
         return jvRequest;
     }
 
