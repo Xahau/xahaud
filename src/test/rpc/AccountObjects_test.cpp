ef3272b3ad1b8386eee15dedd0dab9827ebc7a5c
Denis Angell <dangell@transia.co>
2023-02-20 06:08:53 -0500

    fixup! account_objects test

--- src/test/rpc/AccountObjects_test.cpp
+++ src/test/rpc/AccountObjects_test.cpp
@@ -513,6 +513,28 @@ public:
             BEAST_EXPECT(ticket[sfTicketSequence.jsonName].asUInt() == 12);
         }
         {
+            // Create a uri token.
+            std::string const uri(maxTokenURILength, '?');
+            Json::Value jfURIToken;
+            jfURIToken[jss::TransactionType] = jss::URITokenMint;
+            jfURIToken[jss::Flags] = tfBurnable;
+            jfURIToken[jss::Account] = gw.human();
+            jfURIToken[jss::uri] = strHex(uri);
+            env(jfURIToken);
+            env.close();
+        }
+        {
+            // Find the uri token.
+            std::string const uri(maxTokenURILength, '?');
+            Json::Value const resp = acct_objs(gw, jss::uri_token);
+            BEAST_EXPECT(acct_objs_is_size(resp, 1));
+
+            auto const& uritoken = resp[jss::result][jss::account_objects][0u];
+            BEAST_EXPECT(uritoken[sfOwner.jsonName] == gw.human());
+            BEAST_EXPECT(uritoken[sfIssuer.jsonName] == gw.human());
+            BEAST_EXPECT(uritoken[sfURI.jsonName] == strHex(uri));
+        }
+        {
             // See how "deletion_blockers_only" handles gw's directory.
             Json::Value params;
             params[jss::account] = gw.human();
