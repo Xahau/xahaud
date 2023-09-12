16a65dc694b574f52c8fcd2c79578746273d1d36
Denis Angell <dangell@transia.co>
2023-09-12 10:27:09 +0200

    add UNLReport Tests

--- Builds/CMake/RippledCore.cmake
+++ Builds/CMake/RippledCore.cmake
@@ -745,6 +745,7 @@ if (tests)
     src/test/app/Transaction_ordering_test.cpp
     src/test/app/TrustAndBalance_test.cpp
     src/test/app/TxQ_test.cpp
+    src/test/app/UNLReport_test.cpp
     src/test/app/URIToken_test.cpp
     src/test/app/ValidatorKeys_test.cpp
     src/test/app/ValidatorList_test.cpp
