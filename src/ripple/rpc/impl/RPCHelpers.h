32f74d6a8d0c1c12829643850ade291c3b177ae9
Peter Chen <34582813+PeterChen13579@users.noreply.github.com>
2023-09-21 16:33:24 -0400

    APIv2(gateway_balances, channel_authorize): update errors (#4618)
    gateway_balances
    * When `account` does not exist in the ledger, return `actNotFound`
      * (Previously, a normal response was returned)
      * Fix #4290
    * When required field(s) are missing, return `invalidParams`
      * (Previously, `invalidHotWallet` was incorrectly returned)
      * Fix #4548
    
    channel_authorize
    * When the specified `key_type` is invalid, return `badKeyType`
      * (Previously, `invalidParams` was returned)
      * Fix #4289
    
    Since these are breaking changes, they apply only to API version 2.
    
    Supersedes #4577

--- src/ripple/rpc/impl/RPCHelpers.h
+++ src/ripple/rpc/impl/RPCHelpers.h
@@ -225,9 +225,6 @@ getSeedFromRPC(Json::Value const& params, Json::Value& error);
 std::optional<Seed>
 parseRippleLibSeed(Json::Value const& params);
 
-std::pair<PublicKey, SecretKey>
-keypairForSignature(Json::Value const& params, Json::Value& error);
-
 /**
  * API version numbers used in API version 1
  */
@@ -311,6 +308,12 @@ getAPIVersionNumber(const Json::Value& value, bool betaEnabled);
     or an RPC error */
 std::variant<std::shared_ptr<Ledger const>, Json::Value>
 getLedgerByContext(RPC::JsonContext& context);
+
+std::pair<PublicKey, SecretKey>
+keypairForSignature(
+    Json::Value const& params,
+    Json::Value& error,
+    uint apiVersion = apiVersionIfUnspecified);
 
 }  // namespace RPC
 }  // namespace ripple
