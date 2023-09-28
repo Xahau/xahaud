b6ec83b7050de264ba4e85883057480e3c24ff7d
Denis Angell <dangell@transia.co>
2023-09-28 19:15:28 +0200

    add guards

--- src/ripple/app/tx/impl/DeleteAccount.cpp
+++ src/ripple/app/tx/impl/DeleteAccount.cpp
@@ -218,8 +218,11 @@ DeleteAccount::preclaim(PreclaimContext const& ctx)
         return terNO_ACCOUNT;
 
     // accounts created via Import are blocked from deletion
-    if (sleAccount->isFieldPresent(sfImportSequence))
-        return tecHAS_OBLIGATIONS;
+    if (ctx.view.rules().enabled(featureImport))
+    {
+        if (sleAccount->isFieldPresent(sfImportSequence))
+            return tecHAS_OBLIGATIONS;
+    }
 
     if (ctx.view.rules().enabled(featureNonFungibleTokensV1))
     {
@@ -253,8 +256,11 @@ DeleteAccount::preclaim(PreclaimContext const& ctx)
     // do not allow the account to be removed if there are hooks installed or one or more hook states
     // when these fields are completely empty the field is made absent so this test is sufficient
     // these fields cannot be populated unless hooks is enabled so the rules do not need to be checked
-    if (sleAccount->isFieldPresent(sfHookNamespaces) || sleAccount->isFieldPresent(sfHooks))
-        return tecHAS_OBLIGATIONS;
+    if (ctx.view.rules().enabled(featureHooks))
+    {
+        if (sleAccount->isFieldPresent(sfHookNamespaces) || sleAccount->isFieldPresent(sfHooks))
+            return tecHAS_OBLIGATIONS;
+    }
     
     // When fixNFTokenRemint is enabled, we don't allow an account to be
     // deleted if <FirstNFTokenSequence + MintedNFTokens> is within 256 of the
