1086ea597a4af5dfdbc65120a82d41f2b0f2348f
Denis Angell <dangell@transia.co>
2023-09-28 18:37:48 +0200

    add import account delete test

--- src/test/app/Import_test.cpp
+++ src/test/app/Import_test.cpp
@@ -226,6 +226,23 @@ class Import_test : public beast::unit_test::suite
     }
 
     void
+    incLgrSeqForAccDel(
+        jtx::Env& env,
+        jtx::Account const& acc,
+        std::uint32_t margin = 0)
+    {
+        int const delta = [&]() -> int {
+            if (env.seq(acc) + 255 > env.current()->seq())
+                return env.seq(acc) - env.current()->seq() + 255 - margin;
+            return 0;
+        }();
+        BEAST_EXPECT(margin == 0 || delta >= 0);
+        for (int i = 0; i < delta; ++i)
+            env.close();
+        BEAST_EXPECT(env.current()->seq() == env.seq(acc) + 255 - margin);
+    }
+
+    void
     testComputeStartingBalance(FeatureBitset features)
     {
         testcase("import header - computeStartingBonus");
@@ -4485,6 +4502,42 @@ class Import_test : public beast::unit_test::suite
     }
 
     void
+    testAccountDelete(FeatureBitset features)
+    {
+        testcase("account delete");
+
+        using namespace test::jtx;
+        using namespace std::literals;
+
+        {
+            test::jtx::Env env{*this, makeNetworkVLConfig(21337, keys)};
+
+            auto const feeDrops = env.current()->fees().base;
+
+            auto const alice = Account("alice");
+            auto const bob = Account("bob");
+            env.fund(XRP(1000), alice, bob);
+            env.close();
+
+            // burn 10'000 xrp
+            auto const master = Account("masterpassphrase");
+            env(noop(master), fee(10'000'000'000), ter(tesSUCCESS));
+            env.close();
+
+            Json::Value const xpop = loadXpop(ImportTCAccountSet::w_seed);
+            Json::Value const tx = import(alice, xpop);
+            env(tx, fee(feeDrops * 10), ter(tesSUCCESS));
+
+            // Close enough ledgers to be able to delete alices's account.
+            incLgrSeqForAccDel(env, alice);
+
+            // alice cannot delete account after import
+            auto const acctDelFee{drops(env.current()->fees().increment)};
+            env(acctdelete(alice, bob), fee(acctDelFee), ter(tecHAS_OBLIGATIONS));
+        }
+    }
+
+    void
     testImportSequence(FeatureBitset features)
     {
         testcase("import sequence");
@@ -5401,6 +5454,7 @@ public:
         testAccountCount(features);
         testHookIssuer(features);
         testImportSequence(features);
+        testAccountDelete(features);
         testMaxSupply(features);
         testMinMax(features);
         testHalving(features - featureOwnerPaysFee);
