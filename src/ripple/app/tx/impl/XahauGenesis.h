3bf3010f50024a0127b2806fb13ca4b665b1bc0b
Richard Holland <richard.holland@starstone.co.nz>
2023-10-09 13:02:51 +0000

    Xahau mainnet distribution

--- src/ripple/app/tx/impl/XahauGenesis.h
+++ src/ripple/app/tx/impl/XahauGenesis.h
@@ -5,9 +5,25 @@ namespace XahauGenesis
 
     using namespace ripple;
 
-    constexpr XRPAmount GenesisAmount          {    940'000 * DROPS_PER_XRP };
-    constexpr XRPAmount InfraAmount            {     10'000 * DROPS_PER_XRP };
-    constexpr XRPAmount ExchangeAmount         {      2'000 * DROPS_PER_XRP };
+   
+    constexpr XRPAmount GenesisAmount          {   1'000'000 * DROPS_PER_XRP };
+    constexpr XRPAmount IntPropAmount          { 172'000'000 * DROPS_PER_XRP };
+    constexpr XRPAmount InfraAmount            {  12'000'000 * DROPS_PER_XRP };
+    constexpr XRPAmount ExchangeAmount         {  12'000'000 * DROPS_PER_XRP };
+    constexpr XRPAmount MarketMakerAmount      {  28'000'000 * DROPS_PER_XRP };
+
+    // Foundation amount is 600MM less everything else
+    //    600'000'000
+    //---------------
+    //-     1'000'000   Genesis Acc Bal
+    //-  6*12'000'000   Infra Amounts
+    //-   172'000'000   Intellectual Property Amount
+    //-    28'000'000   Market Maker Amount
+    //-  3*12'000'000   Exchange Amounts
+    //===============  
+    //    291'000'000   
+
+    constexpr XRPAmount FoundationAmount       { 291'000'000 * DROPS_PER_XRP };
 
     // For the Governance Hook: HookOn is set to ttINVOKE only    
     inline
@@ -30,11 +46,16 @@ namespace XahauGenesis
             {{'I', 'R', 'D'}, {0x00U, 0x80U, 0x6AU, 0xACU, 0xAFU, 0x3CU, 0x09U, 0x56U}}
     };
 
+    // These are initial distributions made to contributors to the network outside of
+    // and beyond the Governance Game.
     inline
     std::vector<std::pair<std::string, XRPAmount>> const
     NonGovernanceDistribution
     {    
-        {"nHUJbfgaAtkbuAdtrqCDerP99PcprcpyqakZTsYpkQdb67aKKyJn", ExchangeAmount},
+        {"nHB6Y311qufjwrpPbkiXc6SrEM2BbsqjJD2GE4xnhabRgYT3qMQb", FoundationAmount},  // Foundation
+        {"rs2dgzYeqYqsk8bvkQR5YPyqsXYcA24MP2",                   ExchangeAmount},    // MEXC           
+        {"rfKsmLP6sTfVGDvga6rW6XbmSFUzc3G9f3",                   ExchangeAmount},    // Bitrue
+        {"rQrQMKhcw3WnptGeWiYSwX5Tz3otyJqPnq",                   ExchangeAmount},    // Uphold
     };
 
     inline
@@ -45,13 +66,14 @@ namespace XahauGenesis
     std::vector<std::pair<std::string, XRPAmount>> const
     L1Membership
     {
-        {"nHDs6fHVnhb4ZbSFWF2dTTPHoZ6Rr39i2UfLotzgf8FKUi7iZdxx", InfraAmount},  // tn4
-        {"nHUvgFxT8EGrXqQZ1rqMw67UtduefbaCHiVtVahk9RXDJP1g1mB4", InfraAmount},  // tn5
-        {"nHU7Vn6co7xEFMBesV7qw7FXE8ucKrhVWQiYZB5oGyMhvqrnZrnJ", InfraAmount},  // tn6
-        {"nHBoJCE3wPgkTcrNPMHyTJFQ2t77EyCAqcBRspFCpL6JhwCm94VZ", InfraAmount},  // tn7
-        {"nHUVv4g47bFMySAZFUKVaXUYEmfiUExSoY4FzwXULNwJRzju4XnQ", InfraAmount},  // tn8
-        {"nHBvr8avSFTz4TFxZvvi4rEJZZtyqE3J6KAAcVWVtifsE7edPM7q", InfraAmount},  // tn9
-        {"nHUH3Z8TRU57zetHbEPr1ynyrJhxQCwrJvNjr4j1SMjYADyW1WWe", InfraAmount},  //tn10
+        {"nHB45nBNgjKMssrRqaNVr2tpCq3t55J5APRRDD6ov1U41JfVFjr6", IntPropAmount},    // XRPL-Labs
+        {"nHB2tHvDXE2GM3Cp9ivyAXU3NDLkf8mzYREQkcZ7wFJyBiaVLu24", InfraAmount},      // Titanium
+        {"nHUetpkzTgf8c6A1DTGemRrhL4nMjsiJrtcsmmg75ufzV5EDMbWU", InfraAmount},      // Evernode
+        {"nHBcpUteNtfXeEquLuXVSAAPKxPigmkLAvmpnPjiiPQ4dWZvkoxi", InfraAmount},      // Digital Governance
+        {"nHB1BVMv96GUa272VehmoCeGM61uLRHZVbxAG1i9kQ9Xzd1KVrh1", MarketMakerAmount},// GateHub
+        {"nHBVzNWFu2yT373tTto5HT1Tybdq2GE8hsdhHdowUDdj9wBieNFo", InfraAmount},      // L2 Table Projects
+        {"nHUzXFEQwBkqHWp71E23fY9QKvEGQqVncJfm8fPxn4bRBYQ3Eee3", InfraAmount},      // L2 Table Community
+        {"nHB1Sjz33Aizc4cdKWvKJDhh5BJAKe19PW7hsveZQq83RUcaxoXA", InfraAmount},      // L2 Table Auditors & Enterprise
     };
 
     // this data structure is used for testing the amendment only,
@@ -67,47 +89,24 @@ namespace XahauGenesis
     const
     L2Membership
     {
-        {"nHU7Vn6co7xEFMBesV7qw7FXE8ucKrhVWQiYZB5oGyMhvqrnZrnJ",
+        {"nHBVzNWFu2yT373tTto5HT1Tybdq2GE8hsdhHdowUDdj9wBieNFo",    // L2 Table Projects
             {
-                "rN4HyiKpUuQM68pohiMPinQnyPdY8tBcWa",
-                "rnsVxwrctvfiPX6wtX5tjYLPMidXFTqWc8"}},
+                "rJFxdrd1BuMeJshRAZBuHP3hex9DjH1nnr",               // Casino Coin
+                "rHMtqVuvEESUhPrsgb8tSa5ghjyoQySfVC"}},             // xSpectar
 
-        {"nHBoJCE3wPgkTcrNPMHyTJFQ2t77EyCAqcBRspFCpL6JhwCm94VZ",
+        {"nHUzXFEQwBkqHWp71E23fY9QKvEGQqVncJfm8fPxn4bRBYQ3Eee3",    // L2 Table Community
             {
-                "rLjrBkBUSRtZJnFfZAbX4SFQSpkk2HcpX8",
-                "rKUt5c9zqGCsZTVhUqrTUNnAbHHo19vtoM",
-                "rJY9NAbesWDGupunxyTvvtL3yWUDrbuCRF"}},
+                "r42noEGvAqfwXnDBebeEPfYVswZVe6CKPo",               // Bithomp
+                "r47tpT8LUoymNgRWzfUq2LdkPRfo4UZSkB",               // Reto
+                "rscan6NzxxSFxEQST8qALrc5v9mpM8fU1j",               // XRPScan
+                "rWiNRBZaEHFptxtkdohBbk36UxoHVwvEa",                // XRPLWin
+                "rQQQrUdN1cLdNmxH4dHfKgmX5P4kf3ZrM",                // Tequ
+                "rxah6E9kpp1Zw98MxYFMoWMQ1NHCZSmyx"}},              // xTokenize
 
-        {"nHUVv4g47bFMySAZFUKVaXUYEmfiUExSoY4FzwXULNwJRzju4XnQ",
+        {"nHB1Sjz33Aizc4cdKWvKJDhh5BJAKe19PW7hsveZQq83RUcaxoXA",    // L2 Table Auditors & Enterprise
             {
-                "r38XdJQ2TKdLRAENjLoft8eDvFsUee5wbr",
-                "rnVtyAEp4TGyje7ccS1SjWHVwxqqQBeft3",
-                "rpzQniG7qsVi6qaS5X2QuscfpWY31j5bks",
-                "rsb7Y9qE7uvftjHZPW1qBVbLdCxjGe5G8X",
-                "rJeoxs1fZW78sMeamwJ27CVcXZNpQZR3t"}},
-
-        {"nHBvr8avSFTz4TFxZvvi4rEJZZtyqE3J6KAAcVWVtifsE7edPM7q",
-            {
-                "rh8svn1EFs3TKEYVBZfWpNdqNr2R1uyM7y",
-                "rMn7PRAiT22hytTpLBHHZ34jPe2RCi2svT",
-                "rLSCctV2Q5rsduFuZk7N65mbSrf3BFknPc",
-                "rn8b9tjZbKmPSoMKV8QwgcU8wq2DVCdfQN",
-                "rEAeU9EDmdcJ3xPLW714bm9fygDV8XoNrj",
-                "rpCLrZYhfaN7ysBNpfAZuNj49Fyg4SHFGv",
-                "rafa8E9RPa5vxZ4rN8s571aWUdbE4oqv7i",
-                "r37Qu8nTfdJFkE14ERAB3LH3wSdz1LbyzU",
-                "rnqXJXh1mGf9BGt3aB74RscNsJiDMV1YPK",
-                "rLhHTgwBbq7aVsrSPp2CDeyX3RRuXihGVv",
-                "rJt6kBV8EjhN9v7U4KnYtydfFu3DaQVCvF",
-                "r4YGLYBzvWTZGLKwbByfVxc8ZjCgBUUwBn",
-                "rEw7zrMdCXFs3CzxtC3NFpjmM2HBLTigVL",
-                "rwrqQBN88MeT3QDipCfJSeJ9sZMZA54xkD",
-                "rpmAcuJAWVgS1zL3R1ob8F5ZSJ9d4jEAoj",
-                "rwGMc2FXtvPitSppNwJaSxqSfEfrLVRtMm",
-                "rUrAvfQTv16EETc3Q2sgCTAoKS9C49crx2",
-                "rBDsW6p9Xak9b2ye2eAgh9JjpubTzeV1ti",
-                "rhGbC5n1qK3Cq3sBbdtKGV5AR3kboXi41K",
-                "rNu4sdVz6d2H37okUeH47ASMwckhzSx3k5"}},
+                "r9EzMEYr5zuRasrfVKdry9JWLbw9mBe6Jg",               // Fyeo
+                "rpuQonHVeMk1qEz9bWMhDRBDSjvLu2gU1B"}},             // Geveo
     };
 
     inline
