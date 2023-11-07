d13fe7fc019e0f86041458d4a728ad940c082c6a
Denis Angell <dangell@transia.co>
2023-11-08 00:14:08 +0100

    fixup

--- release.sh
+++ release.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
+umask 0000;
 
 # Create a temporary container
 container_id=$(docker create transia/xahaud-binary:$1)
 
 # Copy the xahaud and release.info files from the container to the host
+git config --global --add safe.directory /data/actions-runner/_work/xahaud/xahaud
 docker cp $container_id:/io/release-build/xahaud /data/builds/$(date +%Y).$(date +%-m).$(date +%-d)-$(git rev-parse --abbrev-ref HEAD)+$2
 docker cp $container_id:/io/release-build/release.info /data/builds/$(date +%Y).$(date +%-m).$(date +%-d)-$(git rev-parse --abbrev-ref HEAD)+$2.releaseinfo
 
