5e3372f88f35dc56176ce1ad7598623858972e23
Denis Angell <dangell@transia.co>
2023-11-20 12:07:42 +0100

    clang-format

--- src/ripple/protocol/impl/STTx.cpp
+++ src/ripple/protocol/impl/STTx.cpp
@@ -228,8 +228,7 @@ STTx::checkSign(
     return Unexpected("Internal signature check failure.");
 }
 
-Json::Value
-STTx::getJson(JsonOptions) const
+Json::Value STTx::getJson(JsonOptions) const
 {
     Json::Value ret = STObject::getJson(JsonOptions::none);
     ret[jss::hash] = to_string(getTransactionID());
