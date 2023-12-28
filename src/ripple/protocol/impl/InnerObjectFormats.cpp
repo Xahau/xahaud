b1e3e746ca79a05ca491d9957cab56f3e8281a59
Richard Holland <richard.holland@starstone.co.nz>
2023-12-28 10:35:54 +0000

    Remit squashed (not compiling)
    Co-authored-by: Denis Angell <dangell@transia.co>

--- src/ripple/protocol/impl/InnerObjectFormats.cpp
+++ src/ripple/protocol/impl/InnerObjectFormats.cpp
@@ -143,6 +143,21 @@ InnerObjectFormats::InnerObjectFormats()
             {sfPublicKey, soeREQUIRED},
             {sfAccount, soeOPTIONAL},
         });
+
+    add(sfAmountEntry.jsonName.c_str(),
+        sfAmountEntry.getCode(),
+        {
+            {sfAmount, soeREQUIRED},
+            {sfFlags, soeOPTIONAL},
+        });
+
+    add(sfMintURIToken.jsonName.c_str(),
+        sfMintURIToken.getCode(),
+        {
+            {sfURI, soeREQUIRED},
+            {sfDigest, soeOPTIONAL},
+            {sfFlags, soeOPTIONAL},
+        });
 }
 
 InnerObjectFormats const&
