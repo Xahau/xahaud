3144e6c97ddfbba41df808694bc5dda204f02deb
Denis Angell <dangell@transia.co>
2024-01-18 10:34:51 +0100

    update govern hook

--- hook/genesis/govern.c
+++ hook/genesis/govern.c
@@ -650,7 +650,14 @@ int64_t hook(uint32_t r)
                         previous_member[3] = tbl;
 
                         uint8_t vote_key[32];
-                        if (state(SBUF(vote_key), SBUF(previous_member)) > 0)
+                        uint8_t ts = 
+                            previous_member[1] == 'H' ? 32 :      // hook topics are a 32 byte hook hash
+                            previous_member[1] == 'S' ? 20 :      // account topics are a 20 byte account ID
+                            8;                        // reward topics are an 8 byte le xfl
+                        
+                        uint8_t padding = 32 - ts;
+
+                        if (state(vote_key + padding, 32, SBUF(previous_member)) == 32)
                         {
                             uint8_t vote_count = 0;
 
