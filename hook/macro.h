37c3d43cb841dcc579997535960f604106a5809f
Denis Angell <dangell@transia.co>
2024-02-03 17:09:33 +0100

    add common macros

--- hook/macro.h
+++ hook/macro.h
@@ -9,12 +9,31 @@
 #ifndef HOOKMACROS_INCLUDED
 #define HOOKMACROS_INCLUDED 1
 
-
 #ifdef NDEBUG
 #define DEBUG 0
 #else
 #define DEBUG 1
 #endif
+
+#define DONEEMPTY()\
+    accept(0,0,__LINE__)
+
+#define DONEMSG(msg)\
+    accept(msg, sizeof(msg),__LINE__)
+
+#define DONE(x)\
+    accept(SVAR(x),(uint32_t)__LINE__);
+
+#define ASSERT(x)\
+{\
+    if (!(x))\
+        rollback(0,0,__LINE__);\
+}
+
+#define NOPE(x)\
+{\
+    return rollback((x), sizeof(x), __LINE__);\
+}
 
 #define TRACEVAR(v) if (DEBUG) trace_num((uint32_t)(#v), (uint32_t)(sizeof(#v) - 1), (int64_t)v);
 #define TRACEHEX(v) if (DEBUG) trace((uint32_t)(#v), (uint32_t)(sizeof(#v) - 1), (uint32_t)(v), (uint32_t)(sizeof(v)), 1);
@@ -26,6 +45,7 @@
 #define GUARDM(maxiter, n) _g(( (1ULL << 31U) + (__LINE__ << 16) + n), (maxiter)+1)
 
 #define SBUF(str) (uint32_t)(str), sizeof(str)
+#define SVAR(x) &x, sizeof(x)
 
 #define REQUIRE(cond, str)\
 {\
@@ -138,6 +158,17 @@ int out_len = 0;\
         *(((uint64_t*)(buf1)) + 2) == *(((uint64_t*)(buf2)) + 2) &&\
         *(((uint64_t*)(buf1)) + 3) == *(((uint64_t*)(buf2)) + 3))
 
+#define BUFFER_EQUAL_64(buf1, buf2) \
+    ( \
+        (*((uint64_t*)(buf1) + 0) == *((uint64_t*)(buf2) + 0)) && \
+        (*((uint64_t*)(buf1) + 1) == *((uint64_t*)(buf2) + 1)) && \
+        (*((uint64_t*)(buf1) + 2) == *((uint64_t*)(buf2) + 2)) && \
+        (*((uint64_t*)(buf1) + 3) == *((uint64_t*)(buf2) + 3)) && \
+        (*((uint64_t*)(buf1) + 4) == *((uint64_t*)(buf2) + 4)) && \
+        (*((uint64_t*)(buf1) + 5) == *((uint64_t*)(buf2) + 5)) && \
+        (*((uint64_t*)(buf1) + 6) == *((uint64_t*)(buf2) + 6)) && \
+        (*((uint64_t*)(buf1) + 7) == *((uint64_t*)(buf2) + 7)) \
+    )
 
 // when using this macro buf1len may be dynamic but buf2len must be static
 // provide n >= 1 to indicate how many times the macro will be hit on the line of code
@@ -185,6 +216,18 @@ int out_len = 0;\
 #define BUFFER_EQUAL(output, buf1, buf2, compare_len)\
     BUFFER_EQUAL_GUARD(output, buf1, compare_len, buf2, compare_len, 1)
 
+
+#define UINT8_TO_BUF(buf_raw, i)\
+{\
+    unsigned char* buf = (unsigned char*)buf_raw;\
+    buf[0] = (((uint8_t)i) >> 0) & 0xFFUL;\
+    if (i < 0) buf[0] |= 0x80U;\
+}
+
+#define UINT8_FROM_BUF(buf)\
+    (((uint8_t)((buf)[0]) <<  0))
+
+
 #define UINT16_TO_BUF(buf_raw, i)\
 {\
     unsigned char* buf = (unsigned char*)buf_raw;\
@@ -205,7 +248,6 @@ int out_len = 0;\
     buf[3] = (((uint64_t)i) >>  0) & 0xFFUL;\
 }
 
-
 #define UINT32_FROM_BUF(buf)\
     (((uint64_t)((buf)[0]) << 24) +\
      ((uint64_t)((buf)[1]) << 16) +\
@@ -225,7 +267,6 @@ int out_len = 0;\
     buf[7] = (((uint64_t)i) >>  0) & 0xFFUL;\
 }
 
-
 #define UINT64_FROM_BUF(buf)\
     (((uint64_t)((buf)[0]) << 56) +\
      ((uint64_t)((buf)[1]) << 48) +\
@@ -235,17 +276,6 @@ int out_len = 0;\
      ((uint64_t)((buf)[5]) << 16) +\
      ((uint64_t)((buf)[6]) <<  8) +\
      ((uint64_t)((buf)[7]) <<  0))
-
-
-#define INT64_FROM_BUF(buf)\
-   ((((uint64_t)((buf)[0] & 0x7FU) << 56) +\
-     ((uint64_t)((buf)[1]) << 48) +\
-     ((uint64_t)((buf)[2]) << 40) +\
-     ((uint64_t)((buf)[3]) << 32) +\
-     ((uint64_t)((buf)[4]) << 24) +\
-     ((uint64_t)((buf)[5]) << 16) +\
-     ((uint64_t)((buf)[6]) <<  8) +\
-     ((uint64_t)((buf)[7]) <<  0)) * (buf[0] & 0x80U ? -1 : 1))
 
 #define INT64_TO_BUF(buf_raw, i)\
 {\
@@ -260,6 +290,65 @@ int out_len = 0;\
     buf[7] = (((uint64_t)i) >>  0) & 0xFFUL;\
     if (i < 0) buf[0] |= 0x80U;\
 }
+
+#define INT64_FROM_BUF(buf)\
+   ((((uint64_t)((buf)[0] & 0x7FU) << 56) +\
+     ((uint64_t)((buf)[1]) << 48) +\
+     ((uint64_t)((buf)[2]) << 40) +\
+     ((uint64_t)((buf)[3]) << 32) +\
+     ((uint64_t)((buf)[4]) << 24) +\
+     ((uint64_t)((buf)[5]) << 16) +\
+     ((uint64_t)((buf)[6]) <<  8) +\
+     ((uint64_t)((buf)[7]) <<  0)) * (buf[0] & 0x80U ? -1 : 1))
+
+
+#define BYTES20_TO_BUF(buf_raw, i)\
+{\
+    unsigned char* buf = (unsigned char*)buf_raw;\
+    *(uint64_t*)(buf + 0) = *(uint64_t*)(i +  0);\
+    *(uint64_t*)(buf + 8) = *(uint64_t*)(i +  8);\
+    *(uint32_t*)(buf + 16) = *(uint32_t*)(i + 16);\
+}
+
+#define BYTES20_FROM_BUF(buf_raw, i)\
+{\
+    const unsigned char* buf = (const unsigned char*)buf_raw;\
+    *(uint64_t*)(i +  0) = *(const uint64_t*)(buf + 0);\
+    *(uint64_t*)(i +  8) = *(const uint64_t*)(buf + 8);\
+    *(uint32_t*)(i + 16) = *(const uint32_t*)(buf + 16);\
+}
+
+#define BYTES32_TO_BUF(buf_raw, i)\
+{\
+    unsigned char* buf = (unsigned char*)buf_raw;\
+    *(uint64_t*)(buf + 0) = *(uint64_t*)(i +  0);\
+    *(uint64_t*)(buf + 8) = *(uint64_t*)(i +  8);\
+    *(uint64_t*)(buf + 16) = *(uint64_t*)(i + 16);\
+    *(uint64_t*)(buf + 24) = *(uint64_t*)(i + 24);\
+}
+
+#define BYTES32_FROM_BUF(buf_raw, i)\
+{\
+    const unsigned char* buf = (const unsigned char*)buf_raw;\
+    *(uint64_t*)(i +  0) = *(const uint64_t*)(buf + 0);\
+    *(uint64_t*)(i +  8) = *(const uint64_t*)(buf + 8);\
+    *(uint64_t*)(i + 16) = *(const uint64_t*)(buf + 16);\
+    *(uint64_t*)(i + 24) = *(const uint64_t*)(buf + 24);\
+}
+
+#define FLIP_ENDIAN_32(n) ((uint32_t) (((n & 0xFFU) << 24U) | \
+                                   ((n & 0xFF00U) << 8U) | \
+                                 ((n & 0xFF0000U) >> 8U) | \
+                                ((n & 0xFF000000U) >> 24U)))
+
+#define FLIP_ENDIAN_64(n) ((uint64_t)(((n & 0xFFULL) << 56ULL) |             \
+                                      ((n & 0xFF00ULL) << 40ULL) |           \
+                                      ((n & 0xFF0000ULL) << 24ULL) |         \
+                                      ((n & 0xFF000000ULL) << 8ULL) |        \
+                                      ((n & 0xFF00000000ULL) >> 8ULL) |      \
+                                      ((n & 0xFF0000000000ULL) >> 24ULL) |   \
+                                      ((n & 0xFF000000000000ULL) >> 40ULL) | \
+                                      ((n & 0xFF00000000000000ULL) >> 56ULL)))
 
 #define tfCANONICAL 0x80000000UL
 
