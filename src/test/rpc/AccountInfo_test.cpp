264f70098e6d227d1ae16d97e9ccb88be419e000
Denis Angell <dangell@transia.co>
2024-02-19 13:37:51 +0100

    add `lsfAllowIncoming` to AccountInfo

--- src/test/rpc/AccountInfo_test.cpp
+++ src/test/rpc/AccountInfo_test.cpp
@@ -546,14 +546,16 @@ public:
         }
 
         static constexpr std::
-            array<std::pair<std::string_view, std::uint32_t>, 4>
+            array<std::pair<std::string_view, std::uint32_t>, 5>
                 disallowIncomingFlags{
                     {{"disallowIncomingCheck", asfDisallowIncomingCheck},
                      {"disallowIncomingNFTokenOffer",
                       asfDisallowIncomingNFTokenOffer},
                      {"disallowIncomingPayChan", asfDisallowIncomingPayChan},
                      {"disallowIncomingTrustline",
-                      asfDisallowIncomingTrustline}}};
+                      asfDisallowIncomingTrustline},
+                     {"allowIncomingRemit",
+                      asfAllowIncomingRemit}}};
 
         if (features[featureDisallowIncoming])
         {
