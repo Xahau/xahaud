63b0245d06ca56770473add8997b4ca18705502c
Denis Angell <dangell@transia.co>
2024-03-25 17:06:39 +0100

    fix muldiv

--- src/ripple/basics/impl/mulDiv.cpp
+++ src/ripple/basics/impl/mulDiv.cpp
@@ -17,21 +17,17 @@
 */
 //==============================================================================
 
-#include <ripple/basics/contract.h>
 #include <ripple/basics/mulDiv.h>
 #include <boost/multiprecision/cpp_int.hpp>
-#include <limits>
-#include <utility>
-
+#include <optional>
 namespace ripple {
 
 std::pair<bool, std::uint64_t>
 mulDiv(std::uint64_t value, std::uint64_t mul, std::uint64_t div)
 {
-    using namespace boost::multiprecision;
+    using uint128_t = boost::multiprecision::uint128_t;
 
-    uint128_t result;
-    result = multiply(result, value, mul);
+    uint128_t result = multiply(result, value, mul);
 
     result /= div;
 
