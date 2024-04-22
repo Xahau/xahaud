ad5a1d2385e2c0abbcdbec57b4b23bccba115d98
seelabs <scott.determan@yahoo.com>
2024-04-22 11:53:47 -0400

    fix amendment: AMM swap should honor invariants: (#5002)
    The AMM has an invariant for swaps where:
    new_balance_1*new_balance_2 >= old_balance_1*old_balance_2
    
    Due to rounding, this invariant could sometimes be violated (although by
    very small amounts).
    
    This patch introduces an amendment `fixAMMRounding` that changes the
    rounding to always favor the AMM. Doing this should maintain the
    invariant.
    
    Co-authored-by: Bronek Kozicki
    Co-authored-by: thejohnfreeman

--- src/ripple/protocol/Feature.h
+++ src/ripple/protocol/Feature.h
@@ -74,7 +74,7 @@ namespace detail {
 // Feature.cpp. Because it's only used to reserve storage, and determine how
 // large to make the FeatureBitset, it MAY be larger. It MUST NOT be less than
 // the actual number of amendments. A LogicError on startup will verify this.
-static constexpr std::size_t numFeatures = 95;
+static constexpr std::size_t numFeatures = 96;
 
 /** Amendments that this server supports and the default voting behavior.
    Whether they are enabled depends on the Rules defined in the validated
@@ -383,6 +383,7 @@ extern uint256 const featurePriceOracle;
 extern uint256 const fixEmptyDID;
 extern uint256 const fixXChainRewardRounding;
 extern uint256 const fixPreviousTxnID;
+extern uint256 const fixAMMRounding;
 
 }  // namespace ripple
 
