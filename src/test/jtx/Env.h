a48d41d3b84f2811b36f3234ae0cf0c0f77d1548
Ed Hennis <ed@ripple.com>
2024-04-24 13:54:46 -0400

    test: Add RPC error checking support to unit tests (#4987)

--- src/test/jtx/Env.h
+++ src/test/jtx/Env.h
@@ -120,6 +120,20 @@ public:
 
     Account const& master = Account::master;
 
+    /// Used by parseResult() and postConditions()
+    struct ParsedResult
+    {
+        std::optional<TER> ter{};
+        // RPC errors tend to return either a "code" and a "message" (sometimes
+        // with an "error" that corresponds to the "code"), or with an "error"
+        // and an "exception". However, this structure allows all possible
+        // combinations.
+        std::optional<error_code_i> rpcCode{};
+        std::string rpcMessage;
+        std::string rpcError;
+        std::string rpcException;
+    };
+
 private:
     struct AppBundle
     {
@@ -510,7 +524,7 @@ public:
 
     /** Gets the TER result and `didApply` flag from a RPC Json result object.
      */
-    static std::pair<TER, bool>
+    static ParsedResult
     parseResult(Json::Value const& jr);
 
     /** Submit an existing JTx.
@@ -531,8 +545,7 @@ public:
     void
     postconditions(
         JTx const& jt,
-        TER ter,
-        bool didApply,
+        ParsedResult const& parsed,
         Json::Value const& jr = Json::Value());
 
     /** Apply funclets and submit. */
--- src/test/protocol/Memo_test.cpp
+++ src/test/protocol/Memo_test.cpp
@@ -56,7 +56,11 @@ public:
             JTx memoSize = makeJtxWithMemo();
             memoSize.jv[sfMemos.jsonName][0u][sfMemo.jsonName]
                        [sfMemoData.jsonName] = std::string(2020, '0');
-            env(memoSize, MEMOFEE, ter(telENV_RPC_FAILED));
+            env(memoSize,
+                MEMOFEE,
+                rpc("invalidTransaction",
+                    "fails local checks: The memo exceeds the maximum allowed "
+                    "size."));
 
             // This memo is just barely small enough.
             memoSize.jv[sfMemos.jsonName][0u][sfMemo.jsonName]
@@ -72,7 +76,11 @@ public:
             auto& m = mi[sfCreatedNode.jsonName];  // CreatedNode in Memos
             m[sfMemoData.jsonName] = "3030303030";
 
-            env(memoNonMemo, MEMOFEE, ter(telENV_RPC_FAILED));
+            env(memoNonMemo,
+                MEMOFEE,
+                rpc("invalidTransaction",
+                    "fails local checks: A memo array may contain only Memo "
+                    "objects."));
         }
         {
             // Put an invalid field in a Memo object.
@@ -80,7 +88,11 @@ public:
             memoExtra
                 .jv[sfMemos.jsonName][0u][sfMemo.jsonName][sfFlags.jsonName] =
                 13;
-            env(memoExtra, MEMOFEE, ter(telENV_RPC_FAILED));
+            env(memoExtra,
+                MEMOFEE,
+                rpc("invalidTransaction",
+                    "fails local checks: A memo may contain only MemoType, "
+                    "MemoData or MemoFormat fields."));
         }
         {
             // Put a character that is not allowed in a URL in a MemoType field.
@@ -88,7 +100,12 @@ public:
             memoBadChar.jv[sfMemos.jsonName][0u][sfMemo.jsonName]
                           [sfMemoType.jsonName] =
                 strHex(std::string_view("ONE<INFINITY"));
-            env(memoBadChar, MEMOFEE, ter(telENV_RPC_FAILED));
+            env(memoBadChar,
+                MEMOFEE,
+                rpc("invalidTransaction",
+                    "fails local checks: The MemoType and MemoFormat fields "
+                    "may only contain characters that are allowed in URLs "
+                    "under RFC 3986."));
         }
         {
             // Put a character that is not allowed in a URL in a MemoData field.
@@ -105,7 +122,12 @@ public:
             memoBadChar.jv[sfMemos.jsonName][0u][sfMemo.jsonName]
                           [sfMemoFormat.jsonName] =
                 strHex(std::string_view("NoBraces{}InURL"));
-            env(memoBadChar, MEMOFEE, ter(telENV_RPC_FAILED));
+            env(memoBadChar,
+                MEMOFEE,
+                rpc("invalidTransaction",
+                    "fails local checks: The MemoType and MemoFormat fields "
+                    "may only contain characters that are allowed in URLs "
+                    "under RFC 3986."));
         }
     }
 
