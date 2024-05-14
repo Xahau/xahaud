cac001b826f5fdd6aab79fdb6eb96ea4d14f336c
Gregory Tsipenyuk <gregtatcam@users.noreply.github.com>
2024-05-14 15:28:38 -0400

    Add the fixAMMOfferRounding amendment: (#4983)
    * Fix AMM offer rounding and low quality LOB offer blocking AMM:
    
    A single-path AMM offer with account offer on DEX, is always generated
    starting with the takerPays first, which is rounded up, and then
    the takerGets, which is rounded down. This rounding ensures that the pool's
    product invariant is maintained. However, when one of the offer's side
    is XRP, this rounding can result in the AMM offer having a lower
    quality, potentially causing offer generation to fail if the quality
    is lower than the account's offer quality.
    
    To address this issue, the proposed fix adjusts the offer generation process
    to start with the XRP side first and always rounds it down. This results
    in a smaller offer size, improving the offer's quality. Regardless if the offer
    has XRP or not, the rounding is done so that the offer size is minimized.
    This change still ensures the product invariant, as the other generated
    side is the exact result of the swap-in or swap-out equations.
    
    If a liquidity can be provided by both AMM and LOB offer on offer crossing
    then AMM offer is generated so that it matches LOB offer quality. If LOB
    offer quality is less than limit quality then generated AMM offer quality
    is also less than limit quality and the offer doesn't cross. To address
    this issue, if LOB quality is better than limit quality then use LOB
    quality to generate AMM offer. Otherwise, don't use the quality to generate
    AMM offer. In this case, limitOut() function in StrandFlow limits
    the out amount to match strand's quality to limit quality and consume
    maximum AMM liquidity.

--- src/ripple/ledger/impl/View.cpp
+++ src/ripple/ledger/impl/View.cpp
@@ -1242,7 +1242,7 @@ accountSend(
     WaiveTransferFee waiveFee,
     bool const senderPaysXferFees)
 {
-    if (view.rules().enabled(fixAMMRounding))
+    if (view.rules().enabled(fixAMMv1_1))
     {
         if (saAmount < beast::zero)
         {
