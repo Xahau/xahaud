XRPLF/rippled

Split tagged cache into .h and .ipp files cc

src/xrpld/app/ledger/ConsensusTransSetSF.cpp
@@ -25,6 +25,7 @@
 #include <xrpld/core/JobQueue.h>
 #include <xrpld/nodestore/Database.h>
 #include <xrpl/basics/Log.h>
+#include <xrpl/basics/TaggedCache.ipp>
 #include <xrpl/protocol/HashPrefix.h>
 #include <xrpl/protocol/digest.h>
 

src/xrpld/app/ledger/detail/TransactionMaster.cpp
@@ -20,6 +20,7 @@
 #include <xrpld/app/ledger/TransactionMaster.h>
 #include <xrpld/app/main/Application.h>
 #include <xrpld/app/misc/Transaction.h>
+#include <xrpl/basics/TaggedCache.ipp>
 #include <xrpl/basics/chrono.h>
 #include <xrpl/protocol/STTx.h>
 

src/xrpld/nodestore/Database.h
@@ -23,7 +23,7 @@
 #include <xrpld/nodestore/Backend.h>
 #include <xrpld/nodestore/NodeObject.h>
 #include <xrpld/nodestore/Scheduler.h>
-#include <xrpl/basics/TaggedCache.h>
+#include <xrpl/basics/TaggedCache.ipp>
 #include <xrpl/protocol/SystemParameters.h>
 
 #include <condition_variable>

src/xrpld/nodestore/detail/DatabaseNodeImp.cpp
@@ -18,6 +18,7 @@
 //==============================================================================
 
 #include <xrpld/nodestore/detail/DatabaseNodeImp.h>
+#include <xrpl/basics/TaggedCache.ipp>
 #include <xrpl/protocol/HashPrefix.h>
 
 namespace ripple {

src/xrpld/shamap/FullBelowCache.h
@@ -22,6 +22,7 @@
 
 #include <xrpl/basics/KeyCache.h>
 #include <xrpl/basics/TaggedCache.h>
+#include <xrpl/basics/TaggedCache.ipp>
 #include <xrpl/basics/base_uint.h>
 #include <xrpl/beast/insight/Collector.h>
 #include <xrpl/beast/utility/Journal.h>

Xahau/xahaud

src/ripple/app/ledger/ConsensusTransSetSF.cpp
@@ -22,9 +22,10 @@
 #include <ripple/app/main/Application.h>
 #include <ripple/app/misc/NetworkOPs.h>
 #include <ripple/app/misc/Transaction.h>
-#include <ripple/basics/Log.h>
 #include <ripple/core/JobQueue.h>
 #include <ripple/nodestore/Database.h>
+#include <ripple/basics/Log.h>
+#include <ripple/basics/TaggedCache.ipp>
 #include <ripple/protocol/HashPrefix.h>
 #include <ripple/protocol/digest.h>
 

src/ripple/app/ledger/impl/TransactionMaster.cpp
@@ -20,6 +20,7 @@
 #include <ripple/app/ledger/TransactionMaster.h>
 #include <ripple/app/main/Application.h>
 #include <ripple/app/misc/Transaction.h>
+#include <ripple/basics/TaggedCache.ipp>
 #include <ripple/basics/chrono.h>
 #include <ripple/protocol/STTx.h>
 

src/ripple/nodestore/Database.h
@@ -20,10 +20,10 @@
 #ifndef RIPPLE_NODESTORE_DATABASE_H_INCLUDED
 #define RIPPLE_NODESTORE_DATABASE_H_INCLUDED
 
-#include <ripple/basics/TaggedCache.h>
 #include <ripple/nodestore/Backend.h>
 #include <ripple/nodestore/NodeObject.h>
 #include <ripple/nodestore/Scheduler.h>
+#include <ripple/basics/TaggedCache.ipp>
 #include <ripple/protocol/SystemParameters.h>
 
 #include <condition_variable>

src/ripple/nodestore/impl/DatabaseNodeImp.cpp
@@ -17,8 +17,8 @@
 */
 //==============================================================================
 
-#include <ripple/app/ledger/Ledger.h>
-#include <ripple/nodestore/impl/DatabaseNodeImp.h>
+#include <ripple/nodestore/detail/DatabaseNodeImp.h>
+#include <ripple/basics/TaggedCache.ipp>
 #include <ripple/protocol/HashPrefix.h>
 
 namespace ripple {

src/ripple/shamap/FullBelowCache.h
@@ -21,7 +21,7 @@
 #define RIPPLE_SHAMAP_FULLBELOWCACHE_H_INCLUDED
 
 #include <ripple/basics/KeyCache.h>
-#include <ripple/basics/TaggedCache.h>
+#include <ripple/basics/TaggedCache.ipp>
 #include <ripple/basics/base_uint.h>
 #include <ripple/beast/insight/Collector.h>
 #include <ripple/beast/utility/Journal.h>