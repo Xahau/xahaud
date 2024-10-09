XRPLF/rippled

Replace SHAMapTreeNodes with intrusive pointers:
This branch has a long history. About two years ago I wrote a patch to
remove the mutex from shamap inner nodes (ref:
https://github.com/seelabs/rippled/tree/lockfree-tagged-cache). At the
time I measured a large memory savings of about 2 gig. Unfortunately,
the code required using the `folly` library, and I was hesitant to
introduce such a large dependency into rippled (especially one that was
so hard to build). This branch resurrects that old work and removes the
`folly` dependency.

The old branch used a lockless atomic shared pointer. This new branch
introduces a intrusive pointer type. Unlike boost's intrusive pointer,
this intrusive pointer can handle both strong and weak pointers (needed
for the tagged cache). Since this is an intrusive pointer type, in order
to support weak pointers, the object is not destroyed when the strong
count goes to zero. Instead, it is "partially destroyed" (for example,
inner nodes will reset their children). This intrusive pointer takes
16-bits for the strong count and 14-bits for the weak count, and takes
one 64-bit pointer to point at the object. This is much smaller than a
std::shared_pointer, which needs a control block to hold the strong and
weak counts (and potentially other objects), as well as an extra pointer
to point at the control block.

The intrusive shared pointer can be modified to support for atomic
operations (there is a branch that adds this support). These atomic
operations can be used instead of the lock when changing inner node
pointers in the shamap.

Note: The space savings is independent from removing the locks from
shamap inner node. Therefor this work is divided into two phases. In the
first phase a non-atomic intrusive pointer is introduced and the locks
are kept. In a second phases the atomic intrusive pointer could be
introduced and the locks will be removed. Some of the code in this patch
is written with the upcoming atomic work in mind (for example, us

src/xrpld/shamap/SHAMapInnerNode.h
@@ -24,6 +24,7 @@
 #include <xrpld/shamap/SHAMapNodeID.h>
 #include <xrpld/shamap/SHAMapTreeNode.h>
 #include <xrpld/shamap/detail/TaggedPointer.h>
+#include <xrpl/basics/IntrusivePointer.h>
 #include <xrpl/basics/TaggedCache.h>
 #include <xrpl/beast/utility/Journal.h>
 
@@ -49,7 +50,7 @@ public:
 private:
     /** Opaque type that contains the `hashes` array (array of type
        `SHAMapHash`) and the `children` array (array of type
-       `std::shared_ptr<SHAMapInnerNode>`).
+       `intr_ptr::SharedPtr<SHAMapInnerNode>`).
      */
     TaggedPointer hashesAndChildren_;
 
@@ -114,7 +115,11 @@ public:
     operator=(SHAMapInnerNode const&) = delete;
     ~SHAMapInnerNode();
 
-    std::shared_ptr<SHAMapTreeNode>
+    // Needed to support intrusive weak pointers
+    void
+    partialDestructor() override;
+
+    intr_ptr::SharedPtr<SHAMapTreeNode>
     clone(std::uint32_t cowid) const override;
 
     SHAMapNodeType
@@ -148,19 +153,20 @@ public:
     getChildHash(int m) const;
 
     void
-    setChild(int m, std::shared_ptr<SHAMapTreeNode> child);
+    setChild(int m, intr_ptr::SharedPtr<SHAMapTreeNode> child);
 
-    void
-    shareChild(int m, std::shared_ptr<SHAMapTreeNode> const& child);
+    template <class T>
+    requires std::derived_from<T, SHAMapTreeNode> void
+    shareChild(int m, SharedIntrusive<T> const& child);
 
     SHAMapTreeNode*
     getChildPointer(int branch);
 
-    std::shared_ptr<SHAMapTreeNode>
+    intr_ptr::SharedPtr<SHAMapTreeNode>
     getChild(int branch);
 
-    std::shared_ptr<SHAMapTreeNode>
-    canonicalizeChild(int branch, std::shared_ptr<SHAMapTreeNode> node);
+    intr_ptr::SharedPtr<SHAMapTreeNode>
+    canonicalizeChild(int branch, intr_ptr::SharedPtr<SHAMapTreeNode> node);
 
     // sync functions
     bool
@@ -188,10 +194,10 @@ public:
     void
     invariants(bool is_root = false) const override;
 
-    static std::shared_ptr<SHAMapTreeNode>
+    static intr_ptr::SharedPtr<SHAMapTreeNode>
     makeFullInner(Slice data, SHAMapHash const& hash, bool hashValid);
 
-    static std::shared_ptr<SHAMapTreeNode>
+    static intr_ptr::SharedPtr<SHAMapTreeNode>
     makeCompressedInner(Slice data);
 };
 

src/xrpld/shamap/SHAMapTreeNode.h
@@ -23,6 +23,8 @@
 #include <xrpld/shamap/SHAMapItem.h>
 #include <xrpld/shamap/SHAMapNodeID.h>
 #include <xrpl/basics/CountedObject.h>
+#include <xrpl/basics/IntrusivePointer.h>
+#include <xrpl/basics/IntrusiveRefCounts.h>
 #include <xrpl/basics/SHAMapHash.h>
 #include <xrpl/basics/TaggedCache.h>
 #include <xrpl/beast/utility/Journal.h>
@@ -50,7 +52,7 @@ enum class SHAMapNodeType {
     tnACCOUNT_STATE = 4
 };
 
-class SHAMapTreeNode
+class SHAMapTreeNode : public IntrusiveRefCounts
 {
 protected:
     SHAMapHash hash_;
@@ -89,15 +91,19 @@ protected:
 public:
     virtual ~SHAMapTreeNode() noexcept = default;
 
+    // Needed to support weak intrusive pointers
+    virtual void
+    partialDestructor(){};
+
     /** \defgroup SHAMap Copy-on-Write Support
 
-        By nature, a node may appear in multiple SHAMap instances. Rather than
-        actually duplicating these nodes, SHAMap opts to be memory efficient
-        and uses copy-on-write semantics for nodes.
+        By nature, a node may appear in multiple SHAMap instances. Rather
+       than actually duplicating these nodes, SHAMap opts to be memory
+       efficient and uses copy-on-write semantics for nodes.
 
-        Only nodes that are not modified and don't need to be flushed back can
-        be shared. Once a node needs to be changed, it must first be copied and
-        the copy must marked as not shareable.
+        Only nodes that are not modified and don't need to be flushed back
+       can be shared. Once a node needs to be changed, it must first be
+       copied and the copy must marked as not shareable.
 
         Note that just because a node may not be *owned* by a given SHAMap
         instance does not mean that the node is NOT a part of any SHAMap. It
@@ -109,8 +115,8 @@ public:
     /** @{ */
     /** Returns the SHAMap that owns this node.
 
-        @return the ID of the SHAMap that owns this node, or 0 if the node
-                is not owned by any SHAMap and is a candidate for sharing.
+        @return the ID of the SHAMap that owns this node, or 0 if the
+       node is not owned by any SHAMap and is a candidate for sharing.
      */
     std::uint32_t
     cowid() const
@@ -130,7 +136,7 @@ public:
     }
 
     /** Make a copy of this node, setting the owner. */
-    virtual std::shared_ptr<SHAMapTreeNode>
+    virtual intr_ptr::SharedPtr<SHAMapTreeNode>
     clone(std::uint32_t cowid) const = 0;
     /** @} */
 
@@ -171,20 +177,20 @@ public:
     virtual void
     invariants(bool is_root = false) const = 0;
 
-    static std::shared_ptr<SHAMapTreeNode>
+    static intr_ptr::SharedPtr<SHAMapTreeNode>
     makeFromPrefix(Slice rawNode, SHAMapHash const& hash);
 
-    static std::shared_ptr<SHAMapTreeNode>
+    static intr_ptr::SharedPtr<SHAMapTreeNode>
     makeFromWire(Slice rawNode);
 
 private:
-    static std::shared_ptr<SHAMapTreeNode>
+    static intr_ptr::SharedPtr<SHAMapTreeNode>
     makeTransaction(Slice data, SHAMapHash const& hash, bool hashValid);
 
-    static std::shared_ptr<SHAMapTreeNode>
+    static intr_ptr::SharedPtr<SHAMapTreeNode>
     makeAccountState(Slice data, SHAMapHash const& hash, bool hashValid);
 
-    static std::shared_ptr<SHAMapTreeNode>
+    static intr_ptr::SharedPtr<SHAMapTreeNode>
     makeTransactionWithMeta(Slice data, SHAMapHash const& hash, bool hashValid);
 };
 

src/xrpld/shamap/detail/NodeFamily.cpp
@@ -21,6 +21,7 @@
 #include <xrpld/app/main/Application.h>
 #include <xrpld/app/main/Tuning.h>
 #include <xrpld/shamap/NodeFamily.h>
+#include <xrpl/basics/TaggedCache.ipp>
 #include <sstream>
 
 namespace ripple {

src/xrpld/shamap/detail/SHAMapLeafNode.cpp
@@ -18,6 +18,7 @@
 //==============================================================================
 
 #include <xrpld/shamap/SHAMapLeafNode.h>
+#include <xrpl/basics/IntrusivePointer.ipp>
 #include <xrpl/basics/contract.h>
 #include <xrpl/beast/core/LexicalCast.h>
 

Xahau/xahaud

src/ripple/shamap/SHAMapInnerNode.h
@@ -20,12 +20,13 @@
 #ifndef RIPPLE_SHAMAP_SHAMAPINNERNODE_H_INCLUDED
 #define RIPPLE_SHAMAP_SHAMAPINNERNODE_H_INCLUDED
 
-#include <ripple/basics/TaggedCache.h>
-#include <ripple/beast/utility/Journal.h>
 #include <ripple/shamap/SHAMapItem.h>
 #include <ripple/shamap/SHAMapNodeID.h>
 #include <ripple/shamap/SHAMapTreeNode.h>
-#include <ripple/shamap/impl/TaggedPointer.h>
+#include <ripple/shamap/detail/TaggedPointer.h>
+#include <ripple/basics/IntrusivePointer.h>
+#include <ripple/basics/TaggedCache.h>
+#include <ripple/beast/utility/Journal.h>
 
 #include <atomic>
 #include <bitset>
@@ -48,7 +49,7 @@ public:
 private:
     /** Opaque type that contains the `hashes` array (array of type
        `SHAMapHash`) and the `children` array (array of type
-       `std::shared_ptr<SHAMapInnerNode>`).
+       `intr_ptr::SharedPtr<SHAMapInnerNode>`).
      */
     TaggedPointer hashesAndChildren_;
 
@@ -113,7 +114,11 @@ public:
     operator=(SHAMapInnerNode const&) = delete;
     ~SHAMapInnerNode();
 
-    std::shared_ptr<SHAMapTreeNode>
+    // Needed to support intrusive weak pointers
+    void
+    partialDestructor() override;
+
+    intr_ptr::SharedPtr<SHAMapTreeNode>
     clone(std::uint32_t cowid) const override;
 
     SHAMapNodeType
@@ -147,19 +152,20 @@ public:
     getChildHash(int m) const;
 
     void
-    setChild(int m, std::shared_ptr<SHAMapTreeNode> child);
+    setChild(int m, intr_ptr::SharedPtr<SHAMapTreeNode> child);
 
-    void
-    shareChild(int m, std::shared_ptr<SHAMapTreeNode> const& child);
+    template <class T>
+    requires std::derived_from<T, SHAMapTreeNode> void
+    shareChild(int m, SharedIntrusive<T> const& child);
 
     SHAMapTreeNode*
     getChildPointer(int branch);
 
-    std::shared_ptr<SHAMapTreeNode>
+    intr_ptr::SharedPtr<SHAMapTreeNode>
     getChild(int branch);
 
-    std::shared_ptr<SHAMapTreeNode>
-    canonicalizeChild(int branch, std::shared_ptr<SHAMapTreeNode> node);
+    intr_ptr::SharedPtr<SHAMapTreeNode>
+    canonicalizeChild(int branch, intr_ptr::SharedPtr<SHAMapTreeNode> node);
 
     // sync functions
     bool
@@ -187,10 +193,10 @@ public:
     void
     invariants(bool is_root = false) const override;
 
-    static std::shared_ptr<SHAMapTreeNode>
+    static intr_ptr::SharedPtr<SHAMapTreeNode>
     makeFullInner(Slice data, SHAMapHash const& hash, bool hashValid);
 
-    static std::shared_ptr<SHAMapTreeNode>
+    static intr_ptr::SharedPtr<SHAMapTreeNode>
     makeCompressedInner(Slice data);
 };
 

src/ripple/shamap/SHAMapTreeNode.h
@@ -20,13 +20,15 @@
 #ifndef RIPPLE_SHAMAP_SHAMAPTREENODE_H_INCLUDED
 #define RIPPLE_SHAMAP_SHAMAPTREENODE_H_INCLUDED
 
+#include <ripple/shamap/SHAMapItem.h>
+#include <ripple/shamap/SHAMapNodeID.h>
 #include <ripple/basics/CountedObject.h>
+#include <ripple/basics/IntrusivePointer.h>
+#include <ripple/basics/IntrusiveRefCounts.h>
 #include <ripple/basics/SHAMapHash.h>
 #include <ripple/basics/TaggedCache.h>
 #include <ripple/beast/utility/Journal.h>
 #include <ripple/protocol/Serializer.h>
-#include <ripple/shamap/SHAMapItem.h>
-#include <ripple/shamap/SHAMapNodeID.h>
 
 #include <cstdint>
 #include <memory>
@@ -50,7 +52,7 @@ enum class SHAMapNodeType {
     tnACCOUNT_STATE = 4
 };
 
-class SHAMapTreeNode
+class SHAMapTreeNode : public IntrusiveRefCounts
 {
 protected:
     SHAMapHash hash_;
@@ -89,15 +91,19 @@ protected:
 public:
     virtual ~SHAMapTreeNode() noexcept = default;
 
+    // Needed to support weak intrusive pointers
+    virtual void
+    partialDestructor(){};
+
     /** \defgroup SHAMap Copy-on-Write Support
 
-        By nature, a node may appear in multiple SHAMap instances. Rather than
-        actually duplicating these nodes, SHAMap opts to be memory efficient
-        and uses copy-on-write semantics for nodes.
+        By nature, a node may appear in multiple SHAMap instances. Rather
+       than actually duplicating these nodes, SHAMap opts to be memory
+       efficient and uses copy-on-write semantics for nodes.
 
-        Only nodes that are not modified and don't need to be flushed back can
-        be shared. Once a node needs to be changed, it must first be copied and
-        the copy must marked as not shareable.
+        Only nodes that are not modified and don't need to be flushed back
+       can be shared. Once a node needs to be changed, it must first be
+       copied and the copy must marked as not shareable.
 
         Note that just because a node may not be *owned* by a given SHAMap
         instance does not mean that the node is NOT a part of any SHAMap. It
@@ -109,8 +115,8 @@ public:
     /** @{ */
     /** Returns the SHAMap that owns this node.
 
-        @return the ID of the SHAMap that owns this node, or 0 if the node
-                is not owned by any SHAMap and is a candidate for sharing.
+        @return the ID of the SHAMap that owns this node, or 0 if the
+       node is not owned by any SHAMap and is a candidate for sharing.
      */
     std::uint32_t
     cowid() const
@@ -130,7 +136,7 @@ public:
     }
 
     /** Make a copy of this node, setting the owner. */
-    virtual std::shared_ptr<SHAMapTreeNode>
+    virtual intr_ptr::SharedPtr<SHAMapTreeNode>
     clone(std::uint32_t cowid) const = 0;
     /** @} */
 
@@ -171,20 +177,20 @@ public:
     virtual void
     invariants(bool is_root = false) const = 0;
 
-    static std::shared_ptr<SHAMapTreeNode>
+    static intr_ptr::SharedPtr<SHAMapTreeNode>
     makeFromPrefix(Slice rawNode, SHAMapHash const& hash);
 
-    static std::shared_ptr<SHAMapTreeNode>
+    static intr_ptr::SharedPtr<SHAMapTreeNode>
     makeFromWire(Slice rawNode);
 
 private:
-    static std::shared_ptr<SHAMapTreeNode>
+    static intr_ptr::SharedPtr<SHAMapTreeNode>
     makeTransaction(Slice data, SHAMapHash const& hash, bool hashValid);
 
-    static std::shared_ptr<SHAMapTreeNode>
+    static intr_ptr::SharedPtr<SHAMapTreeNode>
     makeAccountState(Slice data, SHAMapHash const& hash, bool hashValid);
 
-    static std::shared_ptr<SHAMapTreeNode>
+    static intr_ptr::SharedPtr<SHAMapTreeNode>
     makeTransactionWithMeta(Slice data, SHAMapHash const& hash, bool hashValid);
 };
 

src/ripple/shamap/impl/NodeFamily.cpp
@@ -21,6 +21,7 @@
 #include <ripple/app/main/Application.h>
 #include <ripple/app/main/Tuning.h>
 #include <ripple/shamap/NodeFamily.h>
+#include <ripple/basics/TaggedCache.ipp>
 #include <sstream>
 
 namespace ripple {

src/ripple/shamap/impl/SHAMapLeafNode.cpp
@@ -17,9 +17,10 @@
 */
 //==============================================================================
 
+#include <ripple/shamap/SHAMapLeafNode.h>
+#include <ripple/basics/IntrusivePointer.ipp>
 #include <ripple/basics/contract.h>
 #include <ripple/beast/core/LexicalCast.h>
-#include <ripple/shamap/SHAMapLeafNode.h>
 
 namespace ripple {
 