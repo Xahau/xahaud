6c1db1ea04d29097fe69d92ce239741500c97fae
Denis Angell <dangell@transia.co>
2024-10-12 14:32:21 +0200

    Update OpenSSL.cmake

--- Builds/CMake/deps/OpenSSL.cmake
+++ Builds/CMake/deps/OpenSSL.cmake
@@ -2,34 +2,128 @@
    NIH dep: openssl
 #]===================================================================]
 
-#[===============================================[
-  OPENSSL_ROOT_DIR is the only variable that
-  FindOpenSSL honors for locating, so convert any
-  OPENSSL_ROOT vars to this
-#]===============================================]
-if (NOT DEFINED OPENSSL_ROOT_DIR)
-  if (DEFINED ENV{OPENSSL_ROOT})
-    set (OPENSSL_ROOT_DIR $ENV{OPENSSL_ROOT})
-  elseif (HOMEBREW)
-    execute_process (COMMAND ${HOMEBREW} --prefix openssl
-      OUTPUT_VARIABLE OPENSSL_ROOT_DIR
-      OUTPUT_STRIP_TRAILING_WHITESPACE)
-  endif ()
-  file (TO_CMAKE_PATH "${OPENSSL_ROOT_DIR}" OPENSSL_ROOT_DIR)
-endif ()
+# Include modules
+include(ExternalProject)
+include(CheckCCompilerFlag)
 
-if (static)
-  set (OPENSSL_USE_STATIC_LIBS ON)
-endif ()
-set (OPENSSL_MSVC_STATIC_RT ON)
-find_package (OpenSSL 1.1.1 REQUIRED)
+check_c_compiler_flag("-mavx512f" COMPILER_SUPPORTS_AVX512)
+option(ENABLE_AVX512 "Enable AVX512 instructions" ${COMPILER_SUPPORTS_AVX512})
+
+if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+    set(OPENSSL_PLATFORM "linux-x86_64")
+    if(ENABLE_AVX512 AND COMPILER_SUPPORTS_AVX512)
+        set(OPENSSL_CFLAGS "-mavx512f")
+    else()
+        set(OPENSSL_CFLAGS "")
+    endif()
+elseif(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
+  if(CMAKE_SYSTEM_PROCESSOR MATCHES "arm64")
+    set(OPENSSL_PLATFORM "darwin64-arm64-cc")
+  else()
+    set(OPENSSL_PLATFORM "darwin64-x86_64-cc")
+    if(ENABLE_AVX512 AND COMPILER_SUPPORTS_AVX512)
+        set(OPENSSL_CFLAGS "-mavx512f")
+    else()
+        set(OPENSSL_CFLAGS "")
+    endif()
+  endif()
+elseif(WIN32)
+    # For Windows, assuming MSVC
+    # OpenSSL requires Perl and NASM for building
+    find_program(PERL perl)
+    if(NOT PERL)
+        message(FATAL_ERROR "Perl not found. Perl is required to build OpenSSL on Windows.")
+    endif()
+    find_program(NASM nasm)
+    if(NOT NASM)
+        message(FATAL_ERROR "NASM not found. NASM is required to build OpenSSL on Windows.")
+    endif()
+
+    if(ENABLE_AVX512 AND COMPILER_SUPPORTS_AVX512)
+        set(OPENSSL_CL "/arch:AVX512")
+    else()
+        set(OPENSSL_CL "")
+    endif()
+
+    set(OPENSSL_PLATFORM "VC-WIN64A")
+else()
+    message(FATAL_ERROR "Unsupported platform: ${CMAKE_SYSTEM_NAME}")
+endif()
+
+ExternalProject_Add(openssl_src
+    PREFIX ${nih_cache_path}
+    GIT_REPOSITORY https://github.com/openssl/openssl.git
+    GIT_TAG openssl-3.3.2
+    CONFIGURE_COMMAND
+      ${nih_cache_path}/src/openssl_src/Configure
+      -DBUILD_SHARED_LIBS=OFF
+      -DBUILD_STATIC_LIBS=ON
+      enable-rmd160
+      no-ssl2
+      ${OPENSSL_PLATFORM}
+    LOG_BUILD ON
+    LOG_CONFIGURE ON
+    LOG_DOWNLOAD ON
+    BUILD_IN_SOURCE 1
+    BUILD_COMMAND $(MAKE) CFLAGS=${OPENSSL_CFLAGS}
+    TEST_COMMAND ""
+    INSTALL_COMMAND ""
+    BUILD_BYPRODUCTS
+      <BINARY_DIR>/libcrypto.a
+      <BINARY_DIR>/libssl.a
+)
+
+ExternalProject_Get_Property (openssl_src BINARY_DIR)
+set (openssl_src_BINARY_DIR "${BINARY_DIR}")
+
+add_library (OpenSSL::Crypto STATIC IMPORTED GLOBAL)
+add_library (OpenSSL::SSL STATIC IMPORTED GLOBAL)
+
+add_dependencies(OpenSSL::Crypto openssl_src)
+add_dependencies(OpenSSL::SSL openssl_src)
+
+if(MSVC)
+    # On Linux/macOS, .a or .so file extensions
+    set_target_properties(OpenSSL::Crypto PROPERTIES
+        IMPORTED_LOCATION_DEBUG
+          "${openssl_src_BINARY_DIR}/libcrypto.lib"
+        IMPORTED_LOCATION_RELEASE
+          "${openssl_src_BINARY_DIR}/libcrypto.lib"
+        INTERFACE_INCLUDE_DIRECTORIES
+          "${openssl_src_BINARY_DIR}/include"
+    )
+    set_target_properties(OpenSSL::SSL PROPERTIES
+        IMPORTED_LOCATION_DEBUG
+          "${openssl_src_BINARY_DIR}/libssl.lib"
+        IMPORTED_LOCATION_RELEASE
+          "${openssl_src_BINARY_DIR}/libssl.lib"
+        INTERFACE_INCLUDE_DIRECTORIES
+          "${openssl_src_BINARY_DIR}/include"
+    )
+else()
+    # On Linux/macOS, .a or .so file extensions
+    set_target_properties(OpenSSL::Crypto PROPERTIES
+        IMPORTED_LOCATION_DEBUG
+          "${openssl_src_BINARY_DIR}/libcrypto.a"
+        IMPORTED_LOCATION_RELEASE
+          "${openssl_src_BINARY_DIR}/libcrypto.a"
+        INTERFACE_INCLUDE_DIRECTORIES
+          "${openssl_src_BINARY_DIR}/include"
+    )
+    set_target_properties(OpenSSL::SSL PROPERTIES
+        IMPORTED_LOCATION_DEBUG
+          "${openssl_src_BINARY_DIR}/libssl.a"
+        IMPORTED_LOCATION_RELEASE
+          "${openssl_src_BINARY_DIR}/libssl.a"
+        INTERFACE_INCLUDE_DIRECTORIES
+          "${openssl_src_BINARY_DIR}/include"
+    )
+endif()
+
 target_link_libraries (ripple_libs
   INTERFACE
     OpenSSL::SSL
     OpenSSL::Crypto)
-# disable SSLv2...this can also be done when building/configuring OpenSSL
-set_target_properties(OpenSSL::SSL PROPERTIES
-    INTERFACE_COMPILE_DEFINITIONS OPENSSL_NO_SSL2)
 #[=========================================================[
    https://gitlab.kitware.com/cmake/cmake/issues/16885
    depending on how openssl is built, it might depend
