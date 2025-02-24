9a6d6e2930e4769a552f8b9155dace1e239f2c37
tequ <git@tequ.dev>
2025-02-24 18:33:21 +0900

    Enhance shell script error handling and debugging on GHA (#447)

--- build-full.sh
+++ build-full.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+#!/bin/bash -u
+# We use set -e and bash with -u to bail on first non zero exit code of any
+# processes launched or upon any unbound variable.
+# We use set -x to print commands before running them to help with
+# debugging.
+set -ex
 
 set -e
 
