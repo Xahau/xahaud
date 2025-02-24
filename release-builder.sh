6bab599975334a9d3d36bdb4dbbebc7f9bcfb8f1
tequ <git@tequ.dev>
2025-02-24 17:17:32 +0900

    Enhance shell script error handling and debugging
    Add -u and -x flags to bash scripts for improved error detection
    and command tracing during build processes

--- release-builder.sh
+++ release-builder.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
-
-set -e
+#!/bin/bash -u
+# We use set -e and bash with -u to bail on first non zero exit code of any
+# processes launched or upon any unbound variable.
+# We use set -x to print commands before running them to help with
+# debugging.
+set -ex
 
 echo "START BUILDING (HOST)"
 
