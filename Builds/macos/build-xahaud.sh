627a56cd5df218fe5ffad1f806dbeb4e47ab65cd
Nicholas Dudfield <ndudfield@gmail.com>
2025-02-28 08:10:24 +0700

    wip: macos CI - cache .nih_c

--- Builds/macos/build-xahaud.sh
+++ Builds/macos/build-xahaud.sh
@@ -38,6 +38,6 @@ else
 fi
 
 cmake --build . \
-                  --target rippled \
-                  --parallel \
-                  -j$CPUS
+      --target rippled \
+      --parallel \
+      -j$CPUS
