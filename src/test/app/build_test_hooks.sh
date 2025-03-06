a0b7d84190316693df08e0477d0340e8bbb4a4cf
Niq Dudfield <ndudfield@gmail.com>
2025-03-06 14:27:17 +0700

    Update src/test/app/build_test_hooks.sh
    Co-authored-by: tequ <git@tequ.dev>

--- src/test/app/build_test_hooks.sh
+++ src/test/app/build_test_hooks.sh
@@ -51,8 +51,8 @@ cat $INPUT_FILE | tr '\n' '\f' |
             echo "/* ==== WASM: $COUNTER ==== */" >> $OUTPUT_FILE
             echo -n '{ R"[test.hook](' >> $OUTPUT_FILE
             cat <<< "$line" | sed -E 's/.{7}$//g' | tr -d '\n' | tr '\f' '\n' >> $OUTPUT_FILE
-            echo ')[test.hook]",' >> SetHook_wasm.h
-            echo "{" >> SetHook_wasm.h
+            echo ')[test.hook]",' >> $OUTPUT_FILE
+            echo "{" >> $OUTPUT_FILE
             WAT=`grep -Eo '\(module' <<< $line | wc -l`
             if [ "$WAT" -eq "0" ]
             then
