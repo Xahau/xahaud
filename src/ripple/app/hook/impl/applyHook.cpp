6128697c9f2b301af7508c5eca807125dfd71758
Nicholas Dudfield <ndudfield@gmail.com>
2025-03-14 15:16:35 +0700

    Handle JS_JSONStringify return types

--- src/ripple/app/hook/impl/applyHook.cpp
+++ src/ripple/app/hook/impl/applyHook.cpp
@@ -1739,13 +1739,20 @@ DEFINE_JS_FUNCTION(int64_t, trace, JSValue msg, JSValue data, JSValue as_hex)
         JSValue sdata = JS_JSONStringify(ctx, data, replacer, JS_UNDEFINED);
         JS_FreeValue(ctx, replacer);
 
-        size_t len;
-        const char* cstr = JS_ToCStringLen(ctx, &len, sdata);
-        if (len > 1023)
-            len = 1023;
-        out += std::string(cstr, len);
-        JS_FreeCString(ctx, cstr);
-        JS_FreeValue(ctx, sdata);
+        if (!(JS_IsException(sdata) || JS_IsUndefined(sdata)))
+        {
+            assert(JS_IsString(sdata));
+            size_t len;
+            const char* cstr = JS_ToCStringLen(ctx, &len, sdata);
+            if (len > 1023)
+                len = 1023;
+            out += std::string(cstr, len);
+            JS_FreeCString(ctx, cstr);
+            JS_FreeValue(ctx, sdata);
+        } else
+        {
+            // No need to free value types
+        }
     }
 
     if (out.size() > 0)
@@ -5115,7 +5122,7 @@ DEFINE_JS_FUNCTION(JSValue, emit, JSValue raw_tx)
         // stringify it
         JSValue sdata =
             JS_JSONStringify(ctx, raw_tx, JS_UNDEFINED, JS_UNDEFINED);
-        if (JS_IsException(sdata))
+        if (JS_IsException(sdata) || JS_IsUndefined(sdata))
             returnJS(INVALID_ARGUMENT);
 
         size_t len;
@@ -5123,6 +5130,7 @@ DEFINE_JS_FUNCTION(JSValue, emit, JSValue raw_tx)
         if (len > 1024 * 1024)
         {
             JS_FreeCString(ctx, cstr);
+            JS_FreeValue(ctx, sdata);
             returnJS(TOO_BIG);
         }
         std::string const tmpl(cstr, len);
@@ -5184,7 +5192,7 @@ DEFINE_JS_FUNCTION(JSValue, prepare, JSValue raw_tmpl)
 
     // stringify it
     JSValue sdata = JS_JSONStringify(ctx, raw_tmpl, JS_UNDEFINED, JS_UNDEFINED);
-    if (JS_IsException(sdata))
+    if (JS_IsException(sdata) || JS_IsUndefined(sdata))
         returnJS(INVALID_ARGUMENT);
     size_t len;
     const char* cstr = JS_ToCStringLen(ctx, &len, sdata);
@@ -5385,7 +5393,7 @@ DEFINE_JS_FUNCTION(JSValue, sto_from_json, JSValue raw_json_in)
         // stringify it
         JSValue sdata =
             JS_JSONStringify(ctx, raw_json_in, JS_UNDEFINED, JS_UNDEFINED);
-        if (JS_IsException(sdata))
+        if (JS_IsException(sdata) || JS_IsUndefined(sdata))
             returnJS(INVALID_ARGUMENT);
 
         const char* cstr = JS_ToCStringLen(ctx, &len, sdata);
