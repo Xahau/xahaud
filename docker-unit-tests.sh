cbfc790f9bdfc296cec724e752e5e1f606be8f8b
tequ <git@tequ.dev>
2025-04-05 02:32:47 +0900

    feat: Run unittests in parallel with Github Actions (#483)
    Implement parallel execution for unit tests using Github Actions to improve CI pipeline efficiency and reduce build times.

--- docker-unit-tests.sh
+++ docker-unit-tests.sh
@@ -1,4 +1,11 @@
-#!/bin/bash
+#!/bin/bash -x
+
+BUILD_CORES=$(echo "scale=0 ; `nproc` / 1.337" | bc)
+
+if [[ "$GITHUB_REPOSITORY" == "" ]]; then
+  #Default
+  BUILD_CORES=8
+fi
 
 echo "Mounting $(pwd)/io in ubuntu and running unit tests"
-docker run --rm -i -v $(pwd):/io ubuntu sh -c '/io/release-build/xahaud -u'
+docker run --rm -i -v $(pwd):/io -e BUILD_CORES=$BUILD_CORES ubuntu sh -c '/io/release-build/xahaud --unittest-jobs $BUILD_CORES -u'
