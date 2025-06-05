a29b1c71eeea7563b96791c808fedad8605faf49
Vlad <129996061+vvysokikh1@users.noreply.github.com>
2025-06-05 14:37:30 +0100

    chore: Remove external project build cores division (#5475)
    The CMake statements that make it seem as if the number of cores used to build external project dependencies is halved don't actually do anything. This change removes these statements.

--- cmake/RippledSanity.cmake
+++ cmake/RippledSanity.cmake
@@ -2,22 +2,7 @@
    convenience variables and sanity checks
 #]===================================================================]
 
-include(ProcessorCount)
-
-if (NOT ep_procs)
-  ProcessorCount(ep_procs)
-  if (ep_procs GREATER 1)
-    # never use more than half of cores for EP builds
-    math (EXPR ep_procs "${ep_procs} / 2")
-    message (STATUS "Using ${ep_procs} cores for ExternalProject builds.")
-  endif ()
-endif ()
-get_property (is_multiconfig GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
-if (is_multiconfig STREQUAL "NOTFOUND")
-  if (${CMAKE_GENERATOR} STREQUAL "Xcode" OR ${CMAKE_GENERATOR} MATCHES "^Visual Studio")
-    set (is_multiconfig TRUE)
-  endif ()
-endif ()
+get_property(is_multiconfig GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
 
 set (CMAKE_CONFIGURATION_TYPES "Debug;Release" CACHE STRING "" FORCE)
 if (NOT is_multiconfig)
