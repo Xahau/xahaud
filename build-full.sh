8e8e42046a7a0c811e57c559e9e1f911f7474f51
Denis Angell <dangell@transia.co>
2025-06-25 09:26:48 +0200

    Update build-full.sh

--- build-full.sh
+++ build-full.sh
@@ -97,6 +97,7 @@ echo "-- Install Cmake 3.25.3 --" &&
 pwd &&
 ( wget -nc -q https://github.com/Kitware/CMake/releases/download/v3.25.3/cmake-3.25.3-linux-x86_64.tar.gz; echo "" ) &&
 tar -xzf cmake-3.25.3-linux-x86_64.tar.gz -C /hbb/ &&
+export PATH="/usr/local/cmake-3.25.3-linux-x86_64/bin:$PATH" &&
 echo "-- Install Boost 1.86.0 --" &&
 pwd &&
 ( wget -nc -q https://archives.boost.io/release/1.86.0/source/boost_1_86_0.tar.gz; echo "" ) &&
