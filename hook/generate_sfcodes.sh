cc62b3cab00f3478f731d60f74bc18dba625e4c9
tequ <git@tequ.dev>
2025-07-16 19:38:51 +0900

    Update Hook headers

--- hook/generate_sfcodes.sh
+++ hook/generate_sfcodes.sh
@@ -1,22 +1,20 @@
-#/bin/bash
-RIPPLED_ROOT="../src/ripple"
+#!/bin/bash
+RIPPLED_ROOT="../include/xrpl"
 echo '// For documentation please see: https://xrpl-hooks.readme.io/reference/'
 echo '// Generated using generate_sfcodes.sh'
-cat $RIPPLED_ROOT/protocol/impl/SField.cpp | grep -E '^CONSTRUCT_' |
+cat $RIPPLED_ROOT/protocol/detail/sfields.macro | grep -E '^(TYPED_SFIELD|UNTYPED_SFIELD)' |
     sed 's/UINT16,/1,/g' |
     sed 's/UINT32,/2,/g' |
     sed 's/UINT64,/3,/g' |
-    sed 's/HASH128,/4,/g' |
-    sed 's/HASH256,/5,/g' |
     sed 's/UINT128,/4,/g' |
     sed 's/UINT256,/5,/g' |
     sed 's/AMOUNT,/6,/g' |
     sed 's/VL,/7,/g' |
     sed 's/ACCOUNT,/8,/g' |
+    sed 's/NUMBER,/9,/g' |
     sed 's/OBJECT,/14,/g' |
     sed 's/ARRAY,/15,/g' |
     sed 's/UINT8,/16,/g' |
-    sed 's/HASH160,/17,/g' |
     sed 's/UINT160,/17,/g' |
     sed 's/PATHSET,/18,/g' |
     sed 's/VECTOR256,/19,/g' |
@@ -24,6 +22,9 @@ cat $RIPPLED_ROOT/protocol/impl/SField.cpp | grep -E '^CONSTRUCT_' |
     sed 's/UINT192,/21,/g' |
     sed 's/UINT384,/22,/g' |
     sed 's/UINT512,/23,/g' |
-    grep -Eo '"([^"]+)", *([0-9]+), *([0-9]+)' |
-    sed 's/"//g' | sed 's/ *//g' | sed 's/,/ /g' |
+    sed 's/ISSUE,/24,/g' |
+    sed 's/XCHAIN_BRIDGE,/25,/g' |
+    sed 's/CURRENCY,/26,/g' |
+    grep -Eo '\(([^,]+), *([0-9]+), *([0-9]+)' |
+    sed s/\(sf//g | sed 's/ *//g' | sed 's/,/ /g' |
     awk '{print ("#define sf"$1" (("$2"U << 16U) + "$3"U)")}'
