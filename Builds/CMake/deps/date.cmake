2225bb4f1266a456a6abac5e24e60c00bb75cb0b
Nicholas Dudfield <ndudfield@gmail.com>
2025-07-26 18:37:46 +0700

    fix: restore release builder date

--- Builds/CMake/deps/date.cmake
+++ Builds/CMake/deps/date.cmake
@@ -9,23 +9,10 @@
 
 find_package (date QUIET)
 if (NOT TARGET date::date)
-  # Build the timezone library
-  set(BUILD_TZ_LIB ON CACHE BOOL "Build date-tz library")
-  set(USE_SYSTEM_TZ_DB ON CACHE BOOL "Use system timezone database")
-  set(ENABLE_DATE_TESTING OFF CACHE BOOL "Disable date tests")
-  
-  # Critical: Disable install/export to prevent CMake errors when used as subdirectory
-  set(ENABLE_DATE_INSTALL OFF CACHE BOOL "Disable date library install")
-  
   FetchContent_Declare(
     hh_date_src
     GIT_REPOSITORY https://github.com/HowardHinnant/date.git
-    GIT_TAG        v3.0.3
+    GIT_TAG        fc4cf092f9674f2670fb9177edcdee870399b829
   )
   FetchContent_MakeAvailable(hh_date_src)
-  
-  # The aliases should already exist in 3.0.4, but ensure they're available
-  if(TARGET date-tz AND NOT TARGET date::date-tz)
-    add_library(date::date-tz ALIAS date-tz)
-  endif()
 endif ()
