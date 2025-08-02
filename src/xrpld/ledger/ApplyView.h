beartec-jpg/FalconLedger

Add Batch feature (XLS-56) (#5060)
- Specification: [XRPLF/XRPL-Standards 56](https://github.com/XRPLF/XRPL-Standards/blob/master/XLS-0056d-batch/README.md)
- Amendment: `Batch`
- Implements execution of multiple transactions within a single batch transaction with four execution modes: `tfAllOrNothing`, `tfOnlyOne`, `tfUntilFailure`, and `tfIndependent`.
- Enables atomic multi-party transactions where multiple accounts can participate in a single batch, with up to 8 inner transactions and 8 batch signers per batch transaction.
- Inner transactions use `tfInnerBatchTxn` flag with zero fees, no signature, and empty signing public key.
- Inner transactions are applied after the outer batch succeeds via the `applyBatchTransactions` function in apply.cpp.
- Network layer prevents relay of transactions with `tfInnerBatchTxn` flag - each peer applies inner transactions locally from the batch.
- Batch transactions are excluded from AccountDelegate permissions but inner transactions retain full delegation support.
- Metadata includes `ParentBatchID` linking inner transactions to their containing batch for traceability and auditing.
- Extended STTx with batch-specific signature verification methods and added protocol structures (`sfRawTransactions`, `sfBatchSigners`).

src/xrpld/ledger/ApplyView.h
@@ -42,6 +42,9 @@ enum ApplyFlags : std::uint32_t {
     // Transaction came from a privileged source
     tapUNLIMITED = 0x400,
 
+    // Transaction is executing as part of a batch
+    tapBATCH = 0x800,
+
     // Transaction shouldn't be applied
     // Signatures shouldn't be checked
     tapDRY_RUN = 0x1000

Xahau/xahaud

src/xrpld/ledger/ApplyView.h
@@ -44,6 +44,9 @@ enum ApplyFlags : std::uint32_t {
     // Transaction is being tested against preflight before emission
     tapPREFLIGHT_EMIT = 0x800,
 
+    // Transaction is executing as part of a batch
+    tapBATCH = 0x200,
+
     // Transaction shouldn't be applied
     // Signatures shouldn't be checked
     tapDRY_RUN = 0x1000