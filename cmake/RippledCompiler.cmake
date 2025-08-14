3bb2937f98b3353cd39fd9b23018227e77af0eec
Bart <bthomee@users.noreply.github.com>
2025-08-14 06:07:09 -0400

    fix: Add -Wno-deprecated-declarations for Clang only (#5680)
    This change adds `-Wno-deprecated-declarations` for Clang only (not for GCC) builds in `cmake/RippledCompiler.cmake`.

--- cmake/RippledCompiler.cmake
+++ cmake/RippledCompiler.cmake
@@ -97,6 +97,7 @@ else ()
     INTERFACE
       -Wall
       -Wdeprecated
+      $<$<BOOL:${is_clang}>:-Wno-deprecated-declarations>
       $<$<BOOL:${wextra}>:-Wextra -Wno-unused-parameter>
       $<$<BOOL:${werr}>:-Werror>
       -fstack-protector
