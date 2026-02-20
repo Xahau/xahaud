908a78a1d9f80a69d9af32efbff1db9bd0f05f12
Nicholas Dudfield <ndudfield@gmail.com>
2026-02-20 10:11:37 +0700

    fix: regenerate hook/extern.h to match hook_api.macro ordering

--- hook/extern.h
+++ hook/extern.h
@@ -330,17 +330,17 @@ extern int64_t
 xpop_slot(uint32_t slot_no_tx, uint32_t slot_no_meta);
 
 extern int64_t
-dice(uint32_t sides);
-
-extern int64_t
-random(uint32_t write_ptr, uint32_t write_len);
-
-extern int64_t
 prepare(
     uint32_t write_ptr,
     uint32_t write_len,
     uint32_t read_ptr,
     uint32_t read_len);
+
+extern int64_t
+dice(uint32_t sides);
+
+extern int64_t
+random(uint32_t write_ptr, uint32_t write_len);
 
 #define HOOK_EXTERN
 #endif  // HOOK_EXTERN
