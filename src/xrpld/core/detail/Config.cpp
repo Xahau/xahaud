e94c98d4a74aa1f825bed0e9c3657b051b7fd244
shortthefomo <508629+shortthefomo@users.noreply.github.com>
2026-03-20 09:29:23 -0300

    fix range issue in config

--- src/xrpld/core/detail/Config.cpp
+++ src/xrpld/core/detail/Config.cpp
@@ -704,6 +704,16 @@ Config::loadFromString(std::string const& fileContents)
                                       ": must be between 10 and 600 inclusive");
     }
 
+    if (getSingleSection(secConfig, SECTION_WORKERS, strTemp, j_))
+    {
+        WORKERS = beast::lexicalCastThrow<int>(strTemp);
+
+        if (WORKERS < 1 || WORKERS > 1024)
+            Throw<std::runtime_error>(
+                "Invalid " SECTION_WORKERS
+                ": must be between 1 and 1024 inclusive.");
+    }
+
     auto const effectiveWorkers = [&]() {
         if (WORKERS)
             return WORKERS;
@@ -729,16 +739,6 @@ Config::loadFromString(std::string const& fileContents)
             "Invalid " SECTION_PATH_WORKERS
             ": must be less than or equal to 3/4 of effective job queue "
             "workers (minimum maximum of 2).");
-
-    if (getSingleSection(secConfig, SECTION_WORKERS, strTemp, j_))
-    {
-        WORKERS = beast::lexicalCastThrow<int>(strTemp);
-
-        if (WORKERS < 1 || WORKERS > 1024)
-            Throw<std::runtime_error>(
-                "Invalid " SECTION_WORKERS
-                ": must be between 1 and 1024 inclusive.");
-    }
 
     if (getSingleSection(secConfig, SECTION_IO_WORKERS, strTemp, j_))
     {
