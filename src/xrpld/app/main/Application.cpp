d845a889a59487b2b3dc685e4e4929d9a2ee2999
shortthefomo <508629+shortthefomo@users.noreply.github.com>
2026-04-15 11:05:53 -0400

    clang-format Application.cpp

--- src/xrpld/app/main/Application.cpp
+++ src/xrpld/app/main/Application.cpp
@@ -314,13 +314,14 @@ public:
         , m_journal(logs_->journal("Application"))
 
         // PerfLog must be started before any other threads are launched.
-        , perfLog_(perf::make_PerfLog(
-              perf::setup_PerfLog(
-                  config_->section("perf"),
-                  config_->CONFIG_DIR),
-              *this,
-              logs_->journal("PerfLog"),
-              [this] { signalStop(); }))
+        , perfLog_(
+              perf::make_PerfLog(
+                  perf::setup_PerfLog(
+                      config_->section("perf"),
+                      config_->CONFIG_DIR),
+                  *this,
+                  logs_->journal("PerfLog"),
+                  [this] { signalStop(); }))
 
         , m_txMaster(*this)
 
@@ -328,13 +329,14 @@ public:
               config_->section(SECTION_INSIGHT),
               logs_->journal("Collector")))
 
-        , m_jobQueue(std::make_unique<JobQueue>(
-              jobQueueThreads(config_),
-              std::min(config_->PATH_WORKERS, maxUpdatePfLimit(config_)),
-              m_collectorManager->group("jobq"),
-              logs_->journal("JobQueue"),
-              *logs_,
-              *perfLog_))
+        , m_jobQueue(
+              std::make_unique<JobQueue>(
+                  jobQueueThreads(config_),
+                  std::min(config_->PATH_WORKERS, maxUpdatePfLimit(config_)),
+                  m_collectorManager->group("jobq"),
+                  logs_->journal("JobQueue"),
+                  *logs_,
+                  *perfLog_))
 
         , m_nodeStoreScheduler(*m_jobQueue)
 
@@ -359,9 +361,10 @@ public:
 
         , validatorKeys_(*config_, m_journal)
 
-        , m_resourceManager(Resource::make_Manager(
-              m_collectorManager->collector(),
-              logs_->journal("Resource")))
+        , m_resourceManager(
+              Resource::make_Manager(
+                  m_collectorManager->collector(),
+                  logs_->journal("Resource")))
 
         , m_nodeStore(m_shaMapStore->makeNodeStore(
               config_->PREFETCH_WORKERS > 0 ? config_->PREFETCH_WORKERS : 4))
@@ -370,16 +373,18 @@ public:
 
         , m_orderBookDB(*this)
 
-        , m_pathRequests(std::make_unique<PathRequests>(
-              *this,
-              logs_->journal("PathRequest"),
-              m_collectorManager->collector()))
+        , m_pathRequests(
+              std::make_unique<PathRequests>(
+                  *this,
+                  logs_->journal("PathRequest"),
+                  m_collectorManager->collector()))
 
-        , m_ledgerMaster(std::make_unique<LedgerMaster>(
-              *this,
-              stopwatch(),
-              m_collectorManager->collector(),
-              logs_->journal("LedgerMaster")))
+        , m_ledgerMaster(
+              std::make_unique<LedgerMaster>(
+                  *this,
+                  stopwatch(),
+                  m_collectorManager->collector(),
+                  logs_->journal("LedgerMaster")))
 
         , ledgerCleaner_(
               make_LedgerCleaner(*this, logs_->journal("LedgerCleaner")))
@@ -399,10 +404,11 @@ public:
                   gotTXSet(set, fromAcquire);
               }))
 
-        , m_ledgerReplayer(std::make_unique<LedgerReplayer>(
-              *this,
-              *m_inboundLedgers,
-              make_PeerSetBuilder(*this)))
+        , m_ledgerReplayer(
+              std::make_unique<LedgerReplayer>(
+                  *this,
+                  *m_inboundLedgers,
+                  make_PeerSetBuilder(*this)))
 
         , m_acceptedLedgerCache(
               "AcceptedLedger",
@@ -426,8 +432,9 @@ public:
 
         , cluster_(std::make_unique<Cluster>(logs_->journal("Overlay")))
 
-        , peerReservations_(std::make_unique<PeerReservationTable>(
-              logs_->journal("PeerReservationTable")))
+        , peerReservations_(
+              std::make_unique<PeerReservationTable>(
+                  logs_->journal("PeerReservationTable")))
 
         , validatorManifests_(
               std::make_unique<ManifestCache>(logs_->journal("ManifestCache")))
@@ -435,13 +442,14 @@ public:
         , publisherManifests_(
               std::make_unique<ManifestCache>(logs_->journal("ManifestCache")))
 
-        , validators_(std::make_unique<ValidatorList>(
-              *validatorManifests_,
-              *publisherManifests_,
-              *timeKeeper_,
-              config_->legacy("database_path"),
-              logs_->journal("ValidatorList"),
-              config_->VALIDATION_QUORUM))
+        , validators_(
+              std::make_unique<ValidatorList>(
+                  *validatorManifests_,
+                  *publisherManifests_,
+                  *timeKeeper_,
+                  config_->legacy("database_path"),
+                  logs_->journal("ValidatorList"),
+                  config_->VALIDATION_QUORUM))
 
         , validatorSites_(std::make_unique<ValidatorSite>(*this))
 
@@ -456,9 +464,10 @@ public:
         , mFeeTrack(
               std::make_unique<LoadFeeTrack>(logs_->journal("LoadManager")))
 
-        , hashRouter_(std::make_unique<HashRouter>(
-              stopwatch(),
-              HashRouter::getDefaultHoldTime()))
+        , hashRouter_(
+              std::make_unique<HashRouter>(
+                  stopwatch(),
+                  HashRouter::getDefaultHoldTime()))
 
         , mValidations(
               ValidationParms(),
