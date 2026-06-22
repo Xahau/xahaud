3fc199017e0cd0faf62eead9638dc2ce3320e062
Nicholas Dudfield <ndudfield@gmail.com>
2026-06-22 16:57:30 +0700

    docs(cmake): note runtime config conan opt-in

--- cmake/RippledSettings.cmake
+++ cmake/RippledSettings.cmake
@@ -15,6 +15,9 @@ option(tests "Build tests" ON)
 option(xahaud_runtime_test_config
   "Enable XAHAUD_RUNTIME_TEST_CONFIG env and runtime_config RPC fault-injection controls"
   OFF)
+# Conan 2 local opt-in:
+# [conf]
+# tools.cmake.cmaketoolchain:extra_variables={"xahaud_runtime_test_config":"ON"}
 
 option(unity "Creates a build using UNITY support in cmake. This is the default" ON)
 if(unity)
