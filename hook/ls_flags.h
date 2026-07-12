558016442dd95b749bdf50d5bc59adfe67fe599c
Nicholas Dudfield <ndudfield@gmail.com>
2026-07-12 10:12:45 +0700

    chore(ci): refresh UNL report generated artifacts

--- hook/ls_flags.h
+++ hook/ls_flags.h
@@ -53,6 +53,10 @@ enum ltNFTOKEN_OFFER {
 enum ltURI_TOKEN {
     lsfBurnable = 0x00000001,
 };
+enum ltUNL_REPORT_MEMBER {
+    lsfUNLReportMemberEquivocationFreeze = 0x00000001,
+    lsfUNLReportMemberSigningKeyCollisionFreeze = 0x00000002,
+};
 enum remarks {
     lsfImmutable = 1,
 };
